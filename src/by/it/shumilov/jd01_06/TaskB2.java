0506c48b3a0a466d8b56766d0d318206510188d9
Shumik <shumik@it-academy.by>
2018-04-24 01:57:03 +0300

    06_TaskB2_1

--- src/by/it/shumilov/jd01_06/TaskB2.java
+++ src/by/it/shumilov/jd01_06/TaskB2.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 
 public class TaskB2 {
     public static void main(String[] args) {
-        String[] mas1  = Poem.text.split("[.?]{1,}");
+        String[] mas1  = Poem.text.split("[.!?]{1,}");
         Pattern pa = Pattern.compile("[\\p{Punct}\\p{Blank}\\n]+");
         Matcher ma ;
         String[] arr =new String[mas1.length];
