417864bee6ca47c705c2448d94163773d44ef737
tarasiukvitali <tarasiukvitali@gmail.com>
2018-04-23 21:14:41 +0300

    jd01_01 taskB exist

---
+++ src/by/it/tarasiuk/jd01_06/TaskB1.java
@@ -0,0 +1,14 @@
+package by.it.tarasiuk.jd01_06;
+
+public class TaskB1 {
+    public static void main(String[] args) {
+//        String[] words =Poem.text.split("[^а-яёА-ЯЁ]+");
+    }
+}
+//        private boolean check(String[] args) {
+//            char first = word.charAt(0);
+//            char last = word.charAt(words.length()-1);
+//            return
+//        }
+//    }
+//}
