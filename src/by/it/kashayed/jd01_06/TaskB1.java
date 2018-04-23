00d403ae9d65e87abdbe3947c8718c84366b740f
Yaroslav <rayrossed@gmail.com>
2018-04-23 21:21:27 +0300

    jd01_06 A1-B1

--- src/by/it/kashayed/jd01_06/TaskB1.java
+++ src/by/it/kashayed/jd01_06/TaskB1.java
@@ -6,7 +6,7 @@ public class TaskB1 {
     private static boolean check(String word){
         char first = word.charAt(0);
         char last = word.charAt(word.length()-1);
-        
+
         return gl.indexOf(first)<0&&gl.indexOf(last)>=0;
     }
     public static void main(String[] args) {
