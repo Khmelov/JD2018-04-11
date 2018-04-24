4b2f4045e82e92b542b7e56a2840e6b19bfcb8ef
Shumik <shumik@it-academy.by>
2018-04-24 22:15:08 +0300

    06_TaskC1

---
+++ src/by/it/shumilov/jd01_06/TaskC1.java
@@ -0,0 +1,39 @@
+package by.it.shumilov.jd01_06;
+
+public class TaskC1 {
+    public static void main(String[] args) {
+        String[] cols = Poem.text.split("\n");
+        int max = 0;
+        for (String col : cols) {
+            if(col.length() > max) max = col.length();
+        }
+        for (String col1 : cols) {
+            System.out.println(plusGap(col1,max));
+        }
+
+    }
+    private  static String plusGap(String s,int max){
+        String[] words = s.split(" ");
+        int nAll = (max-s.length())/(words.length-1);
+        int nOst  = (max-s.length())%(words.length-1);
+        String stroka = words[0];
+
+        String g = "";
+
+        for (int i = 0; i < nAll; i++) {
+            g+= " ";
+        }
+
+
+        for (int i = 1; i < nOst+1; i++) {
+            stroka += "  " + g + words[i];
+        }
+        //if(nOst == 0) nOst = 1;
+        for (int i = nOst+1; i < words.length; i++) {
+            stroka += " " + g + words[i];
+        }
+
+
+        return stroka;
+    }
+}
