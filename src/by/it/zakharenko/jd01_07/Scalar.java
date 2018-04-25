a9f7357f00126bad3f69c0b14276b0788eb7d21a
alinazakharenko <alinazaharenko@gmail.com>
2018-04-25 21:17:33 +0300

    jd01_07

---
+++ src/by/it/zakharenko/jd01_07/Scalar.java
@@ -0,0 +1,24 @@
+package by.it.zakharenko.jd01_07;
+
+public class Scalar extends AbstractVar {
+
+    private double value;
+
+    Scalar(double value) {
+        this.value = value;
+    }
+
+    Scalar(Scalar otherScalar) {
+        this.value = otherScalar.value;
+    }
+
+    Scalar(String strValue) {
+        this.value = Double.parseDouble(strValue);
+    }
+
+    @Override
+    public String toString() {
+        return Double.toString(value);
+        // return "Это скаляр";
+    }
+}
