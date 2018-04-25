1ab1abb1ad58fb4644751c79d068da6e8fc3208a
tayanovskii <tayanovskii@gmail.com>
2018-04-25 21:17:08 +0300

    jd01_07(25.04.2018)

---
+++ src/by/it/tayanovskii/jd01_07/Runner.java
@@ -0,0 +1,4 @@
+package by.it.tayanovskii.jd01_07;
+
+public class Runner {
+}
---
+++ src/by/it/tayanovskii/jd01_07/Vector.java
@@ -0,0 +1,27 @@
+package by.it.tayanovskii.jd01_07;
+
+class Vector extends  AbstractVar{
+    private double [] value;
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("{");
+        String delimiter="";
+        for (double element : value) {
+            sb.append(delimiter).append(element);
+            delimiter=", ";
+        }
+        sb.append('}');
+        return sb.toString();
+    }
+
+    Vector (String strVector)
+    {
+        String[] strValues = strVector.replaceAll("\\{|}", "").trim().split(",\\s*");
+        value=new double[strValues.length];
+        for (int i = 0; i < value.length; i++) {
+            value[i]=Double.parseDouble(strValues[i]);
+        }
+
+    }
+}
