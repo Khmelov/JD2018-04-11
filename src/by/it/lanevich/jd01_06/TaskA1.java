425239fb69b21a57dd326682123c34cecacd3447
vnlanevich <vnlanevich@gmail.com>
2018-04-25 14:36:01 +0300

    На проверку jd01_06

--- src/by/it/lanevich/jd01_06/TaskA1.java
+++ src/by/it/lanevich/jd01_06/TaskA1.java
@@ -6,13 +6,16 @@ import java.util.regex.Pattern;
 class TaskA1 {
     public static void main(String[] args) {
         StringBuilder sb=new StringBuilder(Poem.text);
-        Pattern p=Pattern.compile("[А-яЁё]{4,}");
+        Pattern p=Pattern.compile("[А-Яа-яЁё]{4,}");
         Matcher m=p.matcher(Poem.text);
         while (m.find()){
-            sb.replace(m.start()+3,m.start()+4,"#");
-            if (m.group().length()>6)
+            sb.setCharAt(m.start()+3, '#');
+            if (m.group().length()>=7)
                 sb.setCharAt(m.start()+6,'#');
+
         }
-        System.out.println(sb.toString());
+        System.out.println(sb);
     }
 }
+//    В каждом слове текста 4-ю и 7-ю буквы заменить символом #. Для слов короче 4
+////        символов корректировку не выполнять. Для слов короче 7 символов заменять только 4-ю букву.
--- src/by/it/lanevich/jd01_06/TaskA2.java
+++ src/by/it/lanevich/jd01_06/TaskA2.java
@@ -1,20 +1,45 @@
 package by.it.lanevich.jd01_06;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TaskA2 {
+
+    private static String [] w={};
+    private static int[] count={};
+    private  static  int pos(String word){
+
+        for (int i = 0; i < w.length; i++) {
+            if(w[i].equals(word))
+                return i;
+        }
+        return -1;
+    }
     public static void main(String[] args) {
-        String[] words = Poem.text.split("[А-яЁё]{4,}");
-        String[] slovar = new String[words.length];
-        Arrays.sort(words[0]);
-        //оптимизировать
-        String word;
-        int count=1;
-        for (int i = 1; i < words.length; i++) {
-            if (!word.get().equals(word[i]))
-                System.out.println(word.get() +"=" + count);
-            word.set(word[i]);
-            count=1;
-        } else count++;
+        Pattern pattern=Pattern.compile("[а-яА-ЯёЁ]+");
+        Matcher matcher=pattern.matcher(Poem.text);
+        while(matcher.find()){
+            String word=matcher.group();
+            int p=pos(word);
+            if(p>=0){
+                count[p]++;
+
+            }
+            else{
+                int last=w.length;
+                w=Arrays.copyOf(w,last+1);
+                w[last]=word;
+                count=Arrays.copyOf(count,last+1);
+                count[last]=1;
+
+            }
+        }
+        for (int i = 0; i < w.length; i++) {
+            System.out.println(w[i]+"="+count[i]);
+        }
+
     }
 }
+//Определить, сколько раз повторяется в тексте каждое слово, которое встречается в
+//        нем, используя массивы. Вывести результаты на консоль в формате слово=повторы
