6be3ace5e5986b2e46455739a2324a5b4caaef67
alinazakharenko <alinazaharenko@gmail.com>
2018-04-25 21:17:33 +0300

    jd01_07

---
+++ src/by/it/zakharenko/jd01_07/AbstractVar.java
@@ -0,0 +1,14 @@
+package by.it.zakharenko.jd01_07;
+
+public abstract class AbstractVar {
+
+    /* public void abstrPubMeth(){
+        System.out.println("pubmeth");
+    }*/
+
+    @Override
+    public String toString() {
+        return "Это абстрактная переменная";
+    }
+
+}
---
+++ src/by/it/zakharenko/jd01_07/Vector.java
@@ -0,0 +1,30 @@
+package by.it.zakharenko.jd01_07;
+
+public class Vector {
+
+    private double[] value;
+
+    Vector (String strVector){
+      String[] strValues = strVector
+              .replaceAll("\\{|", "")
+              .trim()
+              .split(",\\s*");
+      value = new double[strValues.length];
+        for (int i = 0; i < value.length; i++) {
+            value[i] = Double.parseDouble(strValues[i]);
+        }
+
+    }
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder("{");
+        String delimiter = "";
+        for (double element : value) {
+            sb.append(delimiter).append(element);
+            delimiter=",";
+
+        }
+        sb.append("}");
+        return sb.toString();
+    }
+}
