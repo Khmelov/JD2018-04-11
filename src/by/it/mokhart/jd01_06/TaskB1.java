bdca8771c665c55915cb1f3d93560b9c8c8a2379
yurymokhart <mokhart.yury@gmail.com>
2018-04-25 17:15:55 +0300

    jd01-06

--- src/by/it/mokhart/jd01_06/TaskB1.java
+++ src/by/it/mokhart/jd01_06/TaskB1.java
@@ -2,12 +2,12 @@ package by.it.mokhart.jd01_06;
 
 public class TaskB1 {
 
-    private final static String gl="уеэоаыяиюёУЕЭОАЫЯИЮЁ";
+    private final static String str = "аоуеиэуыяюёУЕЭОАЫЯИЮЁ";
 
-    private static boolean check(String word){
+    private static boolean check(String word) {
         char first = word.charAt(0);
         char last = word.charAt(word.length() - 1);
-        return gl.indexOf(first)<0 && gl.indexOf(last)>=0;
+        return str.indexOf(first) < 0 && str.indexOf(last) >= 0;
     }
 
     public static void main(String[] args) {
