a70d1949533fe7897084f3c90327d52d980f23ba
tarasiukvitali <tarasiukvitali@gmail.com>
2018-04-30 20:56:47 +0300

    jd01_06 Tasks A1,A2 complete

--- src/by/it/tarasiuk/jd01_06/TaskA1.java
+++ src/by/it/tarasiuk/jd01_06/TaskA1.java
@@ -5,16 +5,14 @@ import java.util.regex.Pattern;
 
 public class TaskA1 {
     public static void main(String[] args) {
-        Pattern pattern=Pattern.compile("[а-яА-ЯёЁ]{4,}");
-        Matcher m = pattern.matcher(Poem.text);
         StringBuilder sb=new StringBuilder(Poem.text);
-        while (m.find()) {
-//            System.out.println(m.group());
-            sb.setCharAt(m.start() + 3,'#');
-            if (m.end()-m.start()>=7)
-            sb.setCharAt(m.start() + 6,'#');
-
+        Pattern pattern= Pattern.compile("[а-яА-ЯёЁ]{4,}");
+        Matcher matcher = pattern.matcher(Poem.text);
+        while (matcher.find()) {
+            sb.setCharAt(matcher.start() + 3,'#');
+            if (matcher.group().length()>=7)
+            sb.setCharAt(matcher.start() + 6,'#');
         }
-        System.out.println(sb.toString());
+        System.out.println(sb);
     }
 }
--- src/by/it/tarasiuk/jd01_06/TaskA2.java
+++ src/by/it/tarasiuk/jd01_06/TaskA2.java
@@ -1,24 +1,42 @@
 package by.it.tarasiuk.jd01_06;
 
+
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TaskA2 {
-    public static void main(String[] args) {
-        String[] words = Poem.text.split("[^а-яёА-ЯЁ]+");
-        Arrays.sort(words);
-        String word = words[0];
-        int count=1;//do-while
-        for (int i = 1; i <words.length ; i++) {
-            if (!word.equals(words[i])){
-                System.out.println(word +"="+count);
-                word=words[i];
-                count=1;
-            }else count++;
 
+    private static String[] words={};
+    private static int[] counter={};
+
+    private static int position(String word) {
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(word))
+                return i;
         }
-//        String[] slovar = new String[words.length];
-//        int[] count = new int[words.length];
+        return -1;
+    }
 
-
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("[а-яА-ЯёЁ]+");
+        Matcher matcher = pattern.matcher(Poem.text);
+        while (matcher.find()) {
+            String word=matcher.group();
+            int p = position(word);
+            if (p>=0) {
+                counter[p]++;
+            }
+            else {
+                int last=words.length;
+                words=Arrays.copyOf(words, last+1);
+                words[last]=word;
+                counter=Arrays.copyOf(counter,last+1);
+                counter[last]=1;
+            }
+        }
+        for (int i = 0; i < words.length; i++) {
+            System.out.println(words[i]+"="+counter[i]);
+        }
     }
 }
