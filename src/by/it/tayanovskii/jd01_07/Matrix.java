959c884cc7f939c8448ce3636119cb20d940b7ce
tayanovskii <tayanovskii@gmail.com>
2018-05-03 23:41:24 +0300

    jd01_05-jd01_07(A,B,C)

--- src/by/it/tayanovskii/jd01_07/Matrix.java
+++ src/by/it/tayanovskii/jd01_07/Matrix.java
@@ -6,6 +6,7 @@ class Matrix extends AbstractVar{
     @Override
     public String toString() {
 
+
         StringBuilder sb = new StringBuilder("{");
         String delimiter="";
 
@@ -13,12 +14,49 @@ class Matrix extends AbstractVar{
             sb.append("{");
             for (int j = 0; j < value.length; j++) {
                 sb.append(delimiter).append(value[i][j]);
-                delimiter=", ";
+                delimiter=",";
             }
             sb.append("}");
+            delimiter="";
+
         }
-        sb.append('}');
-        return sb.toString();
+        sb.append("}");
+        return sb.toString().replaceAll("}\\{", "},{");
 
     }
+    Matrix(double[ ][ ] value)
+    {
+
+        this.value  = new double[value[0].length][value.length];
+
+        for (int i = 0; i < value[0].length; i++) {
+            for (int j = 0; j < value.length; j++) {
+               this.value[i][j]=value[i][j];
+            }
+        }
+
+    }
+
+    Matrix(String strMatrix)
+    {
+
+        String[] rowValues = strMatrix.trim().split("},\\{");
+
+        for (int i = 0; i < rowValues.length; i++) {
+            String[] strValues = rowValues[i].replaceAll("\\{|}", "").trim().split(",");
+            if(value==null)
+                value = new double[rowValues.length][strValues.length];
+            for (int j = 0; j < strValues.length; j++) {
+                value[i][j] = Double.parseDouble(strValues[j]);
+            }
+        }
+
+    }
+
+    Matrix(Matrix matrix)
+    {
+        this.value=matrix.value;
+    }
+
+
 }
--- src/by/it/tayanovskii/jd01_07/Runner.java
+++ src/by/it/tayanovskii/jd01_07/Runner.java
@@ -1,4 +1,11 @@
 package by.it.tayanovskii.jd01_07;
 
 public class Runner {
+
+    public static void main(String[] args) {
+
+        Matrix m = new Matrix("{{1,2},{3,4},{1,2}}");
+    }
+
+
 }
