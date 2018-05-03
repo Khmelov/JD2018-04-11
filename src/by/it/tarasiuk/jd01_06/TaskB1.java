e17e4af648e3d73154e200d45a0ce6cce5cb9b94
tarasiukvitali <tarasiukvitali@gmail.com>
2018-05-03 20:04:21 +0300

    jd01_06 Tasks B1, B2 complete

--- src/by/it/tarasiuk/jd01_06/TaskB1.java
+++ src/by/it/tarasiuk/jd01_06/TaskB1.java
@@ -2,13 +2,16 @@ package by.it.tarasiuk.jd01_06;
 
 public class TaskB1 {
     public static void main(String[] args) {
-//        String[] words =Poem.text.split("[^а-яёА-ЯЁ]+");
+        String[] words=Poem.text.split("[^а-яёА-ЯЁ]+");
+        for (String word : words) {
+            if (check(word)) System.out.println(word);
+        }
+    }
+
+    private static boolean check (String word) {
+        String vowels="аеёиоуыэюяАЕЁИОУЫЭЮЯ";
+        char firstLetter=word.charAt(0);
+        char lastLetter=word.charAt(word.length()-1);
+        return (vowels.indexOf(firstLetter)<0)&&(vowels.indexOf(lastLetter)>=0);
     }
 }
-//        private boolean check(String[] args) {
-//            char first = word.charAt(0);
-//            char last = word.charAt(words.length()-1);
-//            return
-//        }
-//    }
-//}
