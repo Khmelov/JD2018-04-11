3c953e1208cc98a8969b0cb2f89dad7cfe80b3c8
PavelKurmaz <kurmaz.pavel@gmail.com>
2018-05-05 00:13:14 +0300

    jd01_08 completed!

--- src/by/it/kurmaz/jd01_08/Matrix.java
+++ src/by/it/kurmaz/jd01_08/Matrix.java
@@ -23,6 +23,105 @@ public class Matrix extends Var {
         }
     }
 
+    @Override
+    public Var add(Var other) {
+        if (other instanceof Matrix) {
+            double[][] matrix = new double[this.value.length][this.value[0].length];
+            for (int i = 0; i < this.value.length; i++) {
+                for (int j = 0; j < this.value[i].length; j++) {
+                    matrix[i][j] = this.value[i][j] + ((Matrix) other).value[i][j];
+                }
+            }
+            return new Matrix(matrix);
+        }
+        else if (other instanceof Scalar) {
+            double[][] matrix = new double[this.value.length][this.value[0].length];
+            for (int i = 0; i < this.value.length; i++) {
+                for (int j = 0; j < this.value[i].length; j++) {
+                    matrix[i][j] = this.value[i][j] + ((Scalar) other).getValue();
+                }
+            }
+            return new Matrix(matrix);
+        }
+        else
+            return super.add(other);
+    }
+
+    @Override
+    public Var sub(Var other) {
+        if (other instanceof Matrix) {
+            double[][] matrix = new double[this.value.length][this.value[0].length];
+            for (int i = 0; i < this.value.length; i++) {
+                for (int j = 0; j < this.value[i].length; j++) {
+                    matrix[i][j] = this.value[i][j] - ((Matrix) other).value[i][j];
+                }
+            }
+            return new Matrix(matrix);
+        }
+        else if (other instanceof Scalar) {
+            double[][] matrix = new double[this.value.length][this.value[0].length];
+            for (int i = 0; i < this.value.length; i++) {
+                for (int j = 0; j < this.value[i].length; j++) {
+                    matrix[i][j] = this.value[i][j] - ((Scalar) other).getValue();
+                }
+            }
+            return new Matrix(matrix);
+        }
+        else
+            return super.sub(other);
+    }
+
+    @Override
+    public Var mul(Var other) {
+        if (other instanceof Matrix){
+            double[][] newMatrix = new double[this.value.length][((Matrix) other).value[0].length];
+            for (int i = 0; i < this.value.length; i++) {
+                for (int j = 0; j < ((Matrix) other).value[0].length; j++) {
+                    for (int k = 0; k < ((Matrix) other).value.length; k++) {
+                        newMatrix[i][j] += this.value[i][k]* ((Matrix) other).value[k][j];
+                    }
+                }
+            }
+            return new Matrix(newMatrix);
+        }
+        else if (other instanceof Scalar) {
+            double[][] matrix = new double[this.value.length][this.value[0].length];
+            for (int i = 0; i < this.value.length; i++) {
+                for (int j = 0; j < this.value[i].length; j++) {
+                    matrix[i][j] = this.value[i][j] * ((Scalar) other).getValue();
+                }
+            }
+            return new Matrix(matrix);
+        }
+        else if (other instanceof Vector) {
+            double[] newMatrix = new double[this.value.length];
+            for (int i = 0; i < this.value.length; i++ ) {
+                for (int j = 0; j < ((Vector) other).getValue().length; j++) {
+                    newMatrix[i] += this.value[i][j] * ((Vector) other).getValue()[j];
+                }
+            }
+            return new Vector (newMatrix);
+        }
+        else
+            return super.mul(other);
+    }
+
+
+    @Override
+    public Var div(Var other) {
+        if (other instanceof Scalar){
+            double[][] matrix = new double[this.value.length][this.value[0].length];
+            for (int i = 0; i < this.value.length; i++) {
+                for (int j = 0; j < this.value[i].length; j++) {
+                    matrix[i][j] = this.value[i][j] / ((Scalar) other).getValue();
+                }
+            }
+            return new Matrix(matrix);
+        }
+        else
+            return super.div(other);
+    }
+
     public String toString () {
         StringBuilder line = new StringBuilder();
         line.append('{');
--- src/by/it/kurmaz/jd01_08/Runner.java
+++ src/by/it/kurmaz/jd01_08/Runner.java
@@ -1,12 +1,15 @@
 package by.it.kurmaz.jd01_08;
 
 public class Runner {
-    private static void print(Var var) {System.out.println(var);}
+
+    private static void print(Var var) {
+        System.out.println(var);
+    }
 
     public static void main(String[] args) {
         Var s = new Scalar(3.0);
         Var v = new Vector(new double[]{1,2,3}); // закомментируйте вектор и/или
-        Var m = new Matrix("{1,2,3},{4,5,6},{7,8,9}"); // матрицу, если вы их не реализовали
+        Var m = new Matrix("{{1,2,3},{4,5,6},{7,8,9}}"); // матрицу, если вы их не реализовали
 /* Уровень сложности A (калькулятор) */
         print(s.add(s)); //выведет в консоль 6.0
         print(s.sub(s)); //выведет в консоль 0.0
