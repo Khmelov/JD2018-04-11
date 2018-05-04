423eeecbebc0d59c3f916872e1e4e46bd2873a6b
AlexandrRogov <rohaualexandr@gmail.com>
2018-05-04 14:14:38 +0300

    Task 06,07

--- src/by/it/rogov/jd01_02/TaskA.java
+++ src/by/it/rogov/jd01_02/TaskA.java
@@ -1,20 +1,19 @@
 package by.it.rogov.jd01_02;
 
 
-
 import java.util.Scanner;
 
 public class TaskA {
 
     public static void main(String[] args) {
 
-        Scanner scanner= new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        int[] mas= new int[10];
+        int[] mas = new int[10];
 
-        for (int i = 0; i <mas.length ; i++) {
+        for (int i = 0; i < mas.length; i++) {
 
-            mas[i]= scanner.nextInt();
+            mas[i] = scanner.nextInt();
 
         }
 
@@ -25,15 +24,15 @@ public class TaskA {
     }
 
     private static void step3(int[] mas) {
-        int min=mas[0];
+        int min = mas[0];
 
         for (int i : mas) {
 
-            if(min> i) min=i;
+            if (min > i) min = i;
         }
-        for (int i = (mas.length-1); i >= 0; i--) {
-            if(mas[i]==min){
-                System.out.print(i+ " ");
+        for (int i = (mas.length - 1); i >= 0; i--) {
+            if (mas[i] == min) {
+                System.out.print(i + " ");
             }
 
         }
@@ -43,17 +42,17 @@ public class TaskA {
 
     private static void step2(int[] mas) {
 
-        double avg=0;
+        double avg = 0;
         for (int elem : mas) {
-            avg+=elem;
+            avg += elem;
 
         }
-         avg = avg/mas.length;
+        avg = avg / mas.length;
 
         for (int element : mas) {
 
-            if(element<avg){
-                System.out.print(element+ " ");
+            if (element < avg) {
+                System.out.print(element + " ");
             }
 
         }
@@ -62,15 +61,15 @@ public class TaskA {
 
     private static void step1(int[] mas) {
 
-        int min=mas[0];
-        int max=mas[0];
+        int min = mas[0];
+        int max = mas[0];
 
         for (int i : mas) {
 
-            if(min> i) min=i;
-            if(max<i) max=i;
+            if (min > i) min = i;
+            if (max < i) max = i;
         }
-        System.out.println(min + " "+ max);
+        System.out.println(min + " " + max);
 
     }
 
