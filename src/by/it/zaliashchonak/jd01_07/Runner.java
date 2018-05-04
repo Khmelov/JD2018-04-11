6ffbb9e340b8ddc61df03f09be0b226e014ca702
a040985 <a040985@gmail.com>
2018-05-04 09:53:46 +0300

    jd01_07 TaskA-C

--- src/by/it/zaliashchonak/jd01_07/Runner.java
+++ src/by/it/zaliashchonak/jd01_07/Runner.java
@@ -10,7 +10,7 @@ public class Runner {
         AbstractVar sc = new Scalar(100);
         AbstractVar str1= new Scalar("12345");
 
-        //
+
         Scalar str2= new Scalar((Scalar)str1);
         System.out.println(d);
         System.out.println(sc);
@@ -26,7 +26,9 @@ public class Runner {
         System.out.println(v1);
         System.out.println(v2);
         System.out.println(v3);
-        /*Матрицы*/
+
+
+        //Матрицы//
         double matr[][]={{1,2,3,4},{3,4,3,4}};
         Matrix m1 = new Matrix(matr);
         Matrix m2 = new Matrix(m1);
@@ -34,6 +36,8 @@ public class Runner {
         System.out.println(m1);
         System.out.println(m2);
         System.out.println(m3);
+
+        System.out.println("test");
 
     }
 }
