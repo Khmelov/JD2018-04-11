e2ff26dcd9f6ee17bae38f39a8513a9a462198e2
tarasiukvitali <tarasiukvitali@gmail.com>
2018-05-07 15:38:57 +0300

    jd01_08 C2 complete

--- src/by/it/tarasiuk/jd01_08/Matrix.java
+++ src/by/it/tarasiuk/jd01_08/Matrix.java
@@ -60,6 +60,38 @@ class Matrix extends Var {
     }
 
     @Override
+    public Var sub(Var other) {
+        if (other instanceof Scalar) {
+            double[][] res=new double[value.length][];
+            for (int i = 0; i < res.length; i++) {
+                res[i] = Arrays.copyOf(value[i], value[i].length);
+            }
+            for (int i = 0; i < res.length; i++) {
+                for (int j = 0; j < res[i].length; j++) {
+                    res[i][j] -= ((Scalar) other).getValue();
+                }
+            }
+            return new Matrix(res);
+        }
+        else if (other instanceof Matrix) {
+            double[][] res=new double[value.length][];
+            for (int i = 0; i < res.length; i++) {
+                res[i] = Arrays.copyOf(value[i], value[i].length);
+            }
+            for (int i = 0; i < res.length; i++) {
+                for (int j = 0; j < res[i].length; j++) {
+                    res[i][j]-=((Matrix) other).value[i][j];
+                }
+            }
+            return new Matrix(res);
+        }
+        else
+            return super.sub(other);
+    }
+
+
+
+    @Override
     public String toString() {
         StringBuilder sb = new StringBuilder("{");
         String delimiter = "";
