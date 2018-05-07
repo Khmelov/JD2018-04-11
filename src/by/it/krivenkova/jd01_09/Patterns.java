dd2bea327e4e33432456998ec7b52eb5a005b0c1
KrivenkovaNatasha <natatokio12@gmail.com>
2018-05-07 21:09:50 +0300

    add jd01_09

---
+++ src/by/it/krivenkova/jd01_09/Patterns.java
@@ -0,0 +1,7 @@
+package by.it.krivenkova.jd01_09;
+
+public class Patterns {
+    final static String OPERATION ="[-+*/]";
+    final static String SCALAR = "-?[0-9]+\\.?[0-9]*";
+    final static String VECTOR = "\\{{";
+}
