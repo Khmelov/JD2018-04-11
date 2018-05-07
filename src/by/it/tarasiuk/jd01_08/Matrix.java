f96507bd2b15a6f24e88c8871af3edcd4d61376e
tarasiukvitali <tarasiukvitali@gmail.com>
2018-05-07 16:05:11 +0300

    jd01_08 C3 complete

--- src/by/it/tarasiuk/jd01_08/Matrix.java
+++ src/by/it/tarasiuk/jd01_08/Matrix.java
@@ -89,7 +89,51 @@ class Matrix extends Var {
             return super.sub(other);
     }
 
-
+    @Override
+    public Var mul(Var other) {
+        if (other instanceof Scalar) {
+            double[][] res=new double[value.length][];
+            for (int i = 0; i < res.length; i++) {
+                res[i] = Arrays.copyOf(value[i], value[i].length);
+            }
+            for (int i = 0; i < res.length; i++) {
+                for (int j = 0; j < res[i].length; j++) {
+                    res[i][j] *= ((Scalar) other).getValue();
+                }
+            }
+            return new Matrix(res);
+        }
+        else if (other instanceof Vector) {
+            double[] res=new double[value.length];
+            double[][] matrix=new double[value.length][];
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i] = Arrays.copyOf(value[i], value[i].length);
+            }
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j <matrix[i].length; j++) {
+                    res[i]+= matrix[i][j]*((Vector) other).getValue()[j];
+                }
+            }
+            return new Vector(res);
+        }
+        else if (other instanceof Matrix) {
+            double[][] res=new double[value.length][((Matrix) other).value[0].length];
+            double[][] matrix=new double[value.length][];
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i] = Arrays.copyOf(value[i], value[i].length);
+            }
+            for (int i = 0; i < res.length; i++) {
+                for (int j = 0; j < res[i].length; j++) {
+                    for (int k = 0; k < res[i].length ; k++) {
+                        res[i][j]+=value[i][k]*((Matrix) other).value[k][j];
+                    }
+                }
+            }
+            return new Matrix(res);
+        }
+        else
+            return super.mul(other);
+    }
 
     @Override
     public String toString() {
