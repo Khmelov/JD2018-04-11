0bc322efe035398dcddb26d440c6ab7c8d6736cb
a040985 <a040985@gmail.com>
2018-05-11 14:21:01 +0300

    jd01_09 TaskA-B

---
+++ src/by/it/zaliashchonak/jd01_09/Patterns.java
@@ -0,0 +1,8 @@
+package by.it.zaliashchonak.jd01_09;
+
+abstract class Patterns {
+    final static String OPERATION = "[-+*/]";
+    final static String SCALAR = "-?[0-9]+\\.?[0-9]*";
+    final static String VECTOR = "\\{(-?[0-9]+\\.?[0-9]*,?)+}";
+    final static String MATRIX = "\\{(\\{(-?[0-9]+\\.?[0-9]*,?)+},?)+}";
+}
