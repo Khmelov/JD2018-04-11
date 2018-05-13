9219017ecd7b629aca9c8aa82a9d6aac82ba7762
tarasiukvitali <tarasiukvitali@gmail.com>
2018-05-14 02:14:54 +0300

    jd01_010 C

--- src/by/it/tarasiuk/jd01_10/Bean.java
+++ src/by/it/tarasiuk/jd01_10/Bean.java
@@ -1,18 +1,18 @@
 package by.it.tarasiuk.jd01_10;
 
-public class Bean {
+class Bean {
 
-    @Param(a=3, b=4)
+    @Param(a=1, b=0)
     double sum(int a, int b) {
         return a + b;
     }
 
-    @Param(a=3, b=4)
+    @Param(a=5, b=2)
     static double max(int a, int b) {
         return a > b ? a : b;
     }
 
-    @Param(a=3, b=4)
+    @Param(a=4, b=3)
     static double min(int a, int b) {
         return a < b ? a : b;
     }
