df56e54c52f1b6d3f7dfd3738591795b60507450
alinazakharenko <alinazaharenko@gmail.com>
2018-05-14 17:01:02 +0300

    jd01-10

--- src/by/it/zakharenko/jd01_10/PrintMath.java
+++ src/by/it/zakharenko/jd01_10/PrintMath.java
@@ -1,5 +1,6 @@
 package by.it.zakharenko.jd01_10;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
@@ -10,16 +11,24 @@ public class PrintMath {
         //structMath.getDeclaredField("E");
         Method[] methods = structMath.getDeclaredMethods();
         for (Method m : methods) {
-
+            int modifiers = m.getModifiers();
+            if (Modifier.isPublic(modifiers)) {
+                String result = m.toString().replaceAll("java.lang.Math.", "");
+                System.out.println(result);
+            }
              /*int modifiers = m.getModifiers();//m.getModifiers().var
             if ((modifiers & Modifier.PUBLIC) == Modifier.PUBLIC) {
                 System.out.println(m);
             }*/
 
-            int methoMod = m.getModifiers();
+           /* int methoMod = m.getModifiers();
             if (Modifier.isPublic(methoMod))
-                System.out.println(m.toString().replace("java.lang.Math", ""));
-
+                System.out.println(m.toString().replace("java.lang.Math", ""));*/
+            Field[] fields = structMath.getFields();
+            for (Field field : fields) {
+                String result = field.toString().replaceAll("java.lang.Math.", "");
+                System.out.println(result);
+            }
         }
     }
 }
