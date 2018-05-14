7bc13f87d4f3380a2bb9dcee4a201e5be127d5af
KrivenkovaNatasha <natatokio12@gmail.com>
2018-05-14 21:12:00 +0300

    add jd01_11

---
+++ src/by/it/krivenkova/jd01_11/Runner.java
@@ -0,0 +1,16 @@
+package by.it.krivenkova.jd01_11;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Runner {
+    public static void main(String[] args) {
+        List<String> arrayList = new ArrayList<>();
+        arrayList.add("Word 1");
+        arrayList.add("Word 2");
+        arrayList.add("Word 3");
+        arrayList.add("Word 4");
+        arrayList.add("Word 5");
+        System.out.println(arrayList);
+    }
+}
