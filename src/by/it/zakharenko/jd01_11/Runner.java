347e200c08c26728648d5c64b8f3725ff32eb6cb
alinazakharenko <alinazaharenko@gmail.com>
2018-05-16 02:24:53 +0300

    jd01-11

--- src/by/it/zakharenko/jd01_11/Runner.java
+++ src/by/it/zakharenko/jd01_11/Runner.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Runner {
     public static void main(String[] args) {
         List<String> arrayList=new ArrayList<>();
-        List<String> myList=new ListA<>();
+        List<String> myList=new ListA<>();  
         arrayList.add("Word_1"); myList.add("Word_1");
         arrayList.add("Word_2"); myList.add("Word_2");
         arrayList.add("Word_3"); myList.add("Word_3");
