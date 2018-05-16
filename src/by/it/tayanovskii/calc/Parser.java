057c1ee8584403424d515267855b1593523b013a
tayanovskii <tayanovskii@gmail.com>
2018-05-16 21:22:32 +0300

    jd01_12(classwork)

---
+++ src/by/it/tayanovskii/calc/Parser.java
@@ -0,0 +1,32 @@
+package by.it.tayanovskii.calc;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class Parser {
+    Var calc(String expression){
+        // два + три
+        expression=expression.trim().replaceAll("\\s+","");
+        String[] operands = expression.split(Patterns.OPERATION);
+        Var two=Var.createVar(operands[1]);
+        if(expression.contains("="))
+        {
+            return Var.saveVar(operands[0],two);
+        }
+        Var one=Var.createVar(operands[0]);
+        if (one==null || two==null)
+            return null; //todo Create error
+        Pattern pattern=Pattern.compile(Patterns.OPERATION);
+        Matcher matcher = pattern.matcher(expression);
+        if (matcher.find()){
+            String operation=matcher.group();
+            switch (operation) {
+                case "+": return one.add(two);
+                case "-": return one.sub(two);
+                case "*": return one.mul(two);
+                case "/": return one.div(two);
+            }
+        }
+        return one; //todo Create error
+    }
+}
