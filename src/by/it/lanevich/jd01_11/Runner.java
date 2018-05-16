a86108a61515382a8678784e1581d44c23a8c4c3
vnlanevich <vnlanevich@gmail.com>
2018-05-16 15:21:23 +0300

    jd01_11.1 на проверку

---
+++ src/by/it/lanevich/jd01_11/Runner.java
@@ -0,0 +1,84 @@
+package by.it.lanevich.jd01_11;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Runner {
+    public static void main(String[] args) {
+
+//        List<String> myList =new ListA<>();
+//        List<String> myList2=new ListB<>();
+//        List<String> arList =new ArrayList<>();
+//        List<String> col= new ArrayList<>();
+//                col.add("Tree");
+//        col.add("Four");
+//        col.add("Finish");
+//
+//
+//        myList.add("First");
+//        myList.add("Two");
+//        myList.add("Four");
+//        myList2.add("First");
+//        myList2.add("Two");
+//        myList2.add("Four");
+//        arList.add("First");
+//        arList.add("Two");
+//        arList.add("Four");
+//        System.out.println("Метод add(T t)");
+//        System.out.println("myList: "+myList+"\nmyList2: "+myList2+"\narList: "+arList);
+//        System.out.println();
+//
+//        myList.add(2,"Tree");
+//        myList.add(0,"Start");
+//        myList2.add(2,"Tree");
+//        myList2.add(0,"Start");
+//        arList.add(2,"Tree");
+//        arList.add(0,"Start");
+//        System.out.println("Метод add(int index, T element)");
+//        System.out.println("myList: "+myList+"\nmyList2: "+myList2+"\narList: "+arList);
+//        System.out.println();
+//
+//        myList.remove("Start");
+//        myList2.remove("Start");
+//        arList.remove("Start");
+//        System.out.println("Метод remove(Object o)");
+//        System.out.println("myList: "+myList+"\nmyList2: "+myList2+"\narList: "+arList);
+//        System.out.println();
+//
+//        myList.remove(3);
+//        myList2.remove(3);
+//        arList.remove(3);
+//        System.out.println("Метод remove(int index))");
+//        System.out.println("myList: "+myList+"\nmyList2: "+myList2+"\narList: "+arList);
+//        System.out.println();
+//
+//        System.out.println("Метод get(int index)");
+//        System.out.println("myList(0): "+myList.get(0)+"\nmyList2: "+myList2.get(0)+"\narList(0): "+arList.get(0));
+//        System.out.println();
+//
+//        System.out.println("Метод set(int index, T element)");
+//        myList2.set(2,"Four");
+//        arList.set(2,"Four");
+//        System.out.println("myList2: "+myList2+"\narList: "+arList);
+//        System.out.println();
+//
+//        System.out.println("Метод addAll(Collection<? extends T> c)");
+//        myList2.addAll(col);
+//        arList.addAll(col);
+//        System.out.println("myList2: "+myList2+"\narList: "+arList);
+//        System.out.println();
+//
+//        System.out.println("Метод addAll(int index, Collection<? extends T> c)");
+//        myList2.addAll(2,col);
+//        arList.addAll(2,col);
+//        System.out.println("myList2: "+myList2+"\narList: "+arList);
+//        System.out.println();
+
+
+
+    }
+
+
+}
