c9fff3193c0b3d433adf0dbb63655de96d55e64f
PavelKurmaz <kurmaz.pavel@gmail.com>
2018-05-17 10:55:56 +0300

    JD01-11 calculator improved; JD01-12 tasks A & B completed;

--- src/by/it/kurmaz/calc/Variables.java
+++ src/by/it/kurmaz/calc/Variables.java
@@ -5,19 +5,13 @@ import java.util.*;
 class Variables {
     static void printHash (HashMap<String, Var> map) {
         Set<Map.Entry<String, Var>> set = map.entrySet();
-        Iterator <Map.Entry<String, Var>> iter = set.iterator();
-        while (iter.hasNext()) {
-            Map.Entry<String, Var> entry = iter.next();
+        for (Map.Entry<String, Var> entry: set) {
             System.out.println(entry.getKey() + " : " + entry.getValue());
         }
     }
 
     static void printSort (HashMap<String, Var> map) {
-        Set<String> keySet = map.keySet();
-        ArrayList<String> list = new ArrayList<>(keySet);
-        list.sort(Comparator.comparing(String::toString));
-        for (String aList: list) {
-            System.out.println("Key: " + aList + " value: " + map.get(aList));
-        }
+        TreeMap<String, Var> treeMap = new TreeMap<>(map);
+        System.out.println(treeMap);
     }
 }
