81222ce626f9c6a223c2bfed9bd9bc9c56710e38
KrivenkovaNatasha <werden12>
2018-05-20 14:17:20 +0300

    jd 01_13

---
+++ src/by/it/krivenkova/jd01_13/TaskA.java
@@ -0,0 +1,29 @@
+package by.it.krivenkova.jd01_13;
+
+import java.util.HashMap;
+
+public class TaskA {
+    public static void main(String[] args) {
+        try {
+            if (Math.random() > 0.5)
+                new HashMap<String, String>(null);
+            else
+                Integer.parseInt("привет");
+        } catch (NullPointerException | NumberFormatException e) {
+            String name = e.getClass().getName();
+            int line = -1;
+            StackTraceElement[] stackTrace = e.getStackTrace();
+            Class<TaskA> clazz=TaskA.class; //наш текущий класс
+            for (int i = 0; i < stackTrace.length; i++) {
+                StackTraceElement element=stackTrace[i];
+                if (clazz.getName().equals(element.getClassName()))
+                {
+                    String strClass=element.getClassName();
+                    System.out.printf(" name: %s\nclass: %s\n line: %d\n", name, strClass, element.getLineNumber());
+                    break;
+                }
+            }
+
+        }
+    }
+}
