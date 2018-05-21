4d4eb1b1b36931245b21e2d247d5e5903467b44e
AlexandrRogov <RohauAlexandr@gmail.com>
2018-05-21 10:23:33 +0300

    Task 13 B compled

--- src/by/it/rogov/jd01_13/TaskB.java
+++ src/by/it/rogov/jd01_13/TaskB.java
@@ -1,7 +1,7 @@
 package by.it.rogov.jd01_13;
 
 
-import java.util.LinkedList;
+
 import java.util.Scanner;
 
 public class TaskB {
@@ -9,23 +9,20 @@ public class TaskB {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String s;
-        LinkedList<Double> nubbers = new LinkedList<>();
-        int index = 1;
+        Double sum=0.0,number,sqrts;
 
-        while (!(s = scanner.next()).equals("end")) {
+
+        while (!(s = scanner.next()).equals("END")) {
             try {
 
-                nubbers.addLast(Double.parseDouble(s));
-                index++;
-                Double x = 0.0;
-                for (Double aDouble : nubbers) {
-                    x = x + aDouble;
-                    if (x < 0)
-                        throw new ArithmeticException();
+                number=(Double.parseDouble(s));
+                sum+=number;
 
-                }
+                if(sum<0) throw new ArithmeticException();
 
-                System.out.printf("number: %s\n sqrt: %s\n ", nubbers.getLast(), Math.sqrt(x));
+                sqrts=Math.sqrt(sum);
+
+                System.out.printf("number: %s\n sqrt: %s\n ",number,sqrts );
 
             } catch (NumberFormatException | ArithmeticException e) {
 
