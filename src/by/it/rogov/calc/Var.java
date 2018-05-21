938198ae307e6e24484a99965c22009855ac6391
AlexandrRogov <RohauAlexandr@gmail.com>
2018-05-21 11:56:04 +0300

    Task 13 B compled with calc A-C

--- src/by/it/rogov/calc/Var.java
+++ src/by/it/rogov/calc/Var.java
@@ -1,18 +1,20 @@
 package by.it.rogov.calc;
 
+import by.it._examples_.jd01_11.Generics.Demo;
+
 import java.util.HashMap;
 import java.util.Map;
 
 class Var implements Operation {
 
-    private static Map<String,Var> vars=new HashMap<>();
+    private static Map<String, Var> vars = new HashMap<>();
 
-    static Var saveVar (String key,Var var){
-            vars.put(key, var);
-            return var;
+    static Var saveVar(String key, Var var) {
+        vars.put(key, var);
+        return var;
     }
 
-    static Var createVar(String strVar){
+    static Var createVar(String strVar) throws CalcException  {
         if (strVar.matches(Patterns.SCALAR))
             return new Scalar(strVar);
         else if (strVar.matches(Patterns.VECTOR))
@@ -21,7 +23,7 @@ class Var implements Operation {
             return new Matrix(strVar);
         else if (vars.containsKey(strVar))
             return vars.get(strVar);
-        return null; //todo Generate Some Error
+        throw  new CalcException("Невозможно создать: "+ strVar);
     }
 
     @Override
@@ -30,26 +32,22 @@ class Var implements Operation {
     }
 
     @Override
-    public Var add(Var other) {
-        System.out.println("Операция сложения "+this+"+"+other+" невозможна");
-        return null;
+    public Var add(Var other) throws CalcException {
+        throw new CalcException("Операция сложения " + this + "+" + other + " невозможна");
     }
 
     @Override
-    public Var sub(Var other) {
-        System.out.println("Операция вычитания "+this+"+"+other+" невозможна");
-        return null;
+    public Var sub(Var other) throws CalcException {
+        throw new CalcException("Операция вычитания " + this + "+" + other + " невозможна");
     }
 
     @Override
-    public Var mul(Var other) {
-        System.out.println("Операция умножения "+this+"+"+other+" невозможна");
-        return null;
+    public Var mul(Var other) throws CalcException {
+        throw new CalcException("Операция умножения " + this + "+" + other + " невозможна");
     }
 
     @Override
-    public Var div(Var other) {
-        System.out.println("Операция деления "+this+"+"+other+" невозможна");
-        return null;
+    public Var div(Var other) throws CalcException {
+        throw new CalcException("Операция деления " + this + "+" + other + " невозможна");
     }
 }
