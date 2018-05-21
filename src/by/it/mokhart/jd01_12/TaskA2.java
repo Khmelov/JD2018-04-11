1c25c2acbde678e6b488712f11b2df2f95d2902e
yurymokhart <mokhart.yury@gmail.com>
2018-05-21 17:01:57 +0300

    jd01_12 A,B

---
+++ src/by/it/mokhart/jd01_12/TaskA2.java
@@ -0,0 +1,29 @@
+package by.it.mokhart.jd01_12;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TaskA2 {
+
+    public static void main(String[] args) {
+        HashSet<Integer> a = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+        TreeSet<Integer> b = new TreeSet<>(Arrays.asList(10, 11, 12, 9, 8, 7, 6, 15));
+
+        System.out.println(getCross(a, b));
+        System.out.println(getUnion(a, b));
+    }
+
+    private static Set<Integer> getCross(Set<Integer> set1, Set<Integer> set2) {
+        Set<Integer> result = new HashSet<>(set2);
+        result.retainAll(set1);
+        return result;
+    }
+
+    private static Set<Integer> getUnion(Set<Integer> set1, Set<Integer> set2) {
+        Set<Integer> result = new HashSet<>(set2);
+        result.addAll(set1);
+        return result;
+    }
+}
---
+++ src/by/it/mokhart/jd01_12/TaskB3.java
@@ -0,0 +1,61 @@
+package by.it.mokhart.jd01_12;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+public class TaskB3 {
+    public static void main(String[] args) {
+        ArrayList<String> peoples = new ArrayList<>();
+        for (int i = 0; i < 4096 ; i++) {
+            String name = "n" + (i+1);
+            peoples.add(name);
+        }
+        LinkedList<String> peoples2 = new LinkedList<>(peoples);
+
+        System.out.println("Array process: " + process(peoples));
+        System.out.println("LinkedList process: " + process(peoples2));
+    }
+
+    private static String process(LinkedList<String> peoples2) {
+        int loop = 0;
+        double start = System.nanoTime();
+        while (peoples2.size() != 1) {
+            ListIterator<String> iter = peoples2.listIterator();
+            while (iter.hasNext()) {
+                loop++;
+                iter.next();
+                if (loop%2 == 0)
+                    iter.remove();
+            }
+        }
+
+        System.out.println("Time taken: " + (System.nanoTime() - start)/1000);
+        return peoples2.iterator().next();
+    }
+
+
+    private static String process(ArrayList<String> peoples) {
+        int counter = peoples.size();
+        int loop = 0;
+        double start = System.nanoTime();
+        while (counter != 1){
+            int removed = 0;
+            Iterator iterator = peoples.iterator();
+            while (iterator.hasNext()) {
+                loop++;
+                iterator.next();
+                if (loop%2 == 0) {
+                    iterator.remove();
+                    removed++;
+                }
+            }
+            counter -= removed;
+            //System.out.println("People left: " + peoples.toString());
+        }
+        System.out.println("Time taken: " + (System.nanoTime() - start)/1000);
+        return peoples.iterator().next();
+    }
+}
+
