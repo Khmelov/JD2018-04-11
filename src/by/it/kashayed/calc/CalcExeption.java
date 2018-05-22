b6757c2a81d6baf83cadad54bdcce2774697e7fd
Yarik <rayrossed@gmail.com>
2018-05-22 19:13:45 +0300

    calc

---
+++ src/by/it/kashayed/calc/CalcExeption.java
@@ -0,0 +1,21 @@
+package by.it.kashayed.calc;
+
+public class CalcExeption extends Exception{
+    public CalcExeption() {
+        super();
+    }
+
+    public CalcExeption(String message) {
+        super("ERROR:"+message);
+    }
+
+    public CalcExeption(String message, Throwable cause) {
+        super("ERROR:"+message, cause);
+    }
+
+    public CalcExeption(Throwable cause) {
+        super(cause);
+    }
+
+
+}
