faa3fa5854d11a1653b723f4c58b5168b3d0507b
sharinvo <sharinvo@gmail.com>
2018-05-23 21:21:31 +0300

    jd01_15 Classwork

---
+++ src/by/it/sharin/jd01_15/TaskA.java
@@ -0,0 +1,32 @@
+package by.it.sharin.jd01_15;
+
+import java.io.*;
+
+public class TaskA {
+    public static void main(String[] args) {
+//        int[][] mas = new int[6][4];
+//        for (int[] row : mas) {
+//            for (int i = 0; i < row.length; i++) {
+//                row[i]=-15+(int)(Math.random()*31);
+//            }
+//        }
+//        String filename = path("matrix.txt");
+//        try (PrintWriter printWriter = new PrintWriter(new FileWriter(filename))) {
+//            for (int[] row : mas) {
+//                for (int i = 0; i < row.length; i++) {
+//                    printWriter.printf("%3d",row[i]);
+//                }
+//                printWriter.println();
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+//            while (reader.ready()) {
+//                System.out.println(reader.readLine());
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+    }
+}
