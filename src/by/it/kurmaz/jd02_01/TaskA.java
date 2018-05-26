5e3140c632fcd89e0da2091f78f5bb66f075418c
PavelKurmaz <kurmaz.pavel@gmail.com>
2018-05-26 19:33:03 +0300

    JD02_01 Task A, B, C completed

--- src/by/it/kurmaz/jd02_01/TaskA.java
+++ src/by/it/kurmaz/jd02_01/TaskA.java
@@ -8,24 +8,49 @@ public class TaskA {
 
     public static void main(String[] args) {
         System.out.println("Shop is opened");
+        Goods.createBucket();
+        System.out.println("Bucket is created");
         int counter = 1;
+        boolean isOld;
+        int second;
+        boolean condition;
         for (int i = 0; i < 120; i++) {
-            int count = Util.getRandom(0, 2);
-            for (int j = 0; j < count; j++) {
-                Buyer buyer = new Buyer("Buyer # " + counter);
-                buyer.start();
-                counter++;
-                Util.sleep(1000);
+            second = i % 60;
+            if ((second % 10) == 0)
+                System.out.println("Second: " + second + " buyers: " + list.size());
+            if (second <= 30)
+                condition = (list.size() < (second + 10));
+            else
+                condition = (list.size()< (40 + (30 - second)));
+            if (condition) {
+                int count = Util.getRandom(1, 2);
+                for (int j = 0; j < count; j++) {
+                    isOld = ((Util.getRandom(1, 4)) % 4 == 0);
+                    Buyer buyer = new Buyer("Buyer # " + counter, isOld);
+                    buyer.start();
+                    counter++;
+                }
+                Util.sleep(1000, false);
             }
-        }
+            else
+                Util.sleep(1000, false);
+            }
 
-        while (list.size() > 0)
+        while (list.size() != 0)
             try {
-                Thread.sleep(1);
+                Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-
+        /*if (list.size() != 0) {
+            for (Buyer buyer : list) {
+                try {
+                    buyer.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }*/
         System.out.println("Shop closed");
     }
 }
