881936d5f15de5f3d793b7707eaf95126d3678ef
demologin <demo@it-academy.by>
2018-05-28 21:19:20 +0300

    classwork02_02

---
+++ src/by/it/akhmelev/jd02_02/Dispatcher.java
@@ -0,0 +1,33 @@
+package by.it.akhmelev.jd02_02;
+
+public class Dispatcher {
+
+    private static final Object countMonitor = new Object();
+
+    private static final Integer planCount = 100;
+    private static volatile int countBuyerInShop = 0;
+    private static volatile int countBuyerComplete = 0;
+
+    static void addBuyer() {
+        synchronized (countMonitor) {
+            countBuyerInShop++;
+        }
+    }
+
+    static void completeBuyer() {
+        synchronized (countMonitor) {
+            countBuyerInShop--;
+            countBuyerComplete++;
+        }
+    }
+
+    static boolean openedMarket(){
+        return (countBuyerComplete<planCount);
+    }
+
+    static boolean planComplete() {
+        return (countBuyerComplete + countBuyerInShop) >= planCount;
+    }
+
+
+}
