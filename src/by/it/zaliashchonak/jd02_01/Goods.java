91861140939bd636d3e076cc828c996c1e51d905
a040985 <a040985@gmail.com>
2018-05-28 16:02:43 +0300

    jd02_01 TaskA-B (B немного недоделал, неуспел)

---
+++ src/by/it/zaliashchonak/jd02_01/Goods.java
@@ -0,0 +1,33 @@
+package by.it.zaliashchonak.jd02_01;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+class Goods extends HashMap<String, Double> {
+
+
+
+    private static Goods goodPrice = new Goods() {
+        {
+            this.put("Товар1", 1.0);
+            this.put("Товар2", 1.3);
+            this.put("Товар3", 0.7);
+            this.put("Товар4", 2.0);
+        }
+    };
+
+    private Goods() {
+
+    }
+
+    static Double getPrice(String name) {
+        return goodPrice.get(name);
+    }
+
+    static String rndGood() {
+        List<String> names = new ArrayList<>(goodPrice.keySet());
+        return names.get(Util.rnd(names.size()));
+    }
+}
---
+++ src/by/it/zaliashchonak/jd02_01/IUseBacket.java
@@ -0,0 +1,9 @@
+package by.it.zaliashchonak.jd02_01;
+
+/**
+ * Created by Andrey on 28.05.2018.
+ */
+public interface IUseBacket {
+    void takeBacket();
+    void putGoodsToBacket();
+}
