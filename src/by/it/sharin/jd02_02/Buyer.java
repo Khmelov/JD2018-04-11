a1bf2c0c21718cbea804d15a58b8fcc241052ebc
sharinvo <sharinvo@gmail.com>
2018-05-28 20:20:19 +0300

    jd02_02 Classwork

---
+++ src/by/it/sharin/jd02_02/Buyer.java
@@ -0,0 +1,38 @@
+package by.it.sharin.jd02_02;
+
+public class Buyer extends Thread implements IBuyer {
+
+    public Buyer(int number) {
+        super("Покупатель №" + number);
+    }
+
+
+    @Override
+    public void run() {
+        enterToMarket();
+        chooseGoods();
+        goOut();
+    }
+
+    @Override
+    public void enterToMarket() {
+        System.out.println(this + " вошел в магазин");
+    }
+
+    @Override
+    public void chooseGoods() {
+        int timeout = Util.rnd(500, 2000);
+        Util.sleep(timeout);
+        System.out.println(this + " выбрал товары");
+    }
+
+    @Override
+    public void goOut() {
+        System.out.println(this + " вышел из магазина");
+    }
+
+    @Override
+    public String toString() {
+        return getName();
+    }
+}
