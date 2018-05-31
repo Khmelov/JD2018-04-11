1f54cc70d71bdd96f13c6d20868e698df026741c
tarasiukvitali <tarasiukvitali@gmail.com>
2018-05-31 22:03:08 +0300

    jd02_01 Варианты A, B, С - только пенсионер

--- src/by/it/tarasiuk/jd02_01/Buyer.java
+++ src/by/it/tarasiuk/jd02_01/Buyer.java
@@ -1,45 +1,66 @@
 package by.it.tarasiuk.jd02_01;
 
-public class Buyer extends Thread implements IBuyer, IUseBasket {
+import java.util.ArrayList;
+import java.util.List;
+
+class Buyer extends Thread implements IBuyer, IUseBasket {
+
+    private Basket basket = new Basket();
+    private boolean pensioner = false;
 
     Buyer(int number) {
         super("Покупатель №" + number);
+        if ((number % 2 == 0) && (Util.rnd(0, 1) == 0)) {
+            pensioner = true;
+        }
     }
-        Basket basket =new Basket();
+
+
     @Override
     public void run() {
         enterToMarket();
         takeBasket();
         chooseGoods();
-        putGoodsToBasket();
         goToOut();
     }
 
     @Override
     public void enterToMarket() {
-        System.out.println(this + " вошел в магазин");
+        if (!pensioner) System.out.println(this + " вошел в магазин");
+        else System.out.println(this + " - пенсионер вошел в магазин");
+
     }
 
     @Override
     public void takeBasket() {
         int timeout = Util.rnd(100, 200);
+        if (pensioner) timeout=Util.rnd(150,300);
         Util.sleep(timeout);
         System.out.println(this + " взял корзину");
     }
 
     @Override
     public void chooseGoods() {
-        int timeout = Util.rnd(500, 2000);
-        Util.sleep(timeout);
-        System.out.println(this + " выбрал товары");
+        int numberOfProducts = Util.rnd(1, 4);
+        List<String> goodsList = new ArrayList<>(Goods.goodsInMarket.keySet());
+        for (int i = 0; i < numberOfProducts; i++) {
+            int timeout = Util.rnd(500, 2000);
+            if (pensioner) timeout=Util.rnd(750,3000);
+            Util.sleep(timeout);
+            String product = goodsList.get(Util.rnd(0, goodsList.size() - 1));
+            System.out.println(this + " выбрал товар: " + product
+                    + ", стоимостью: " + Goods.goodsInMarket.get(product));
+            putGoodsToBasket(product);
+        }
     }
 
     @Override
-    public void putGoodsToBasket() {
+    public void putGoodsToBasket(String product) {
         int timeout = Util.rnd(100, 200);
+        if (pensioner) timeout=Util.rnd(150,300);
         Util.sleep(timeout);
-
-
+        basket.addProduct(product);
+        System.out.println(this + " положил " + product + " в корзину");
     }
 
     @Override
