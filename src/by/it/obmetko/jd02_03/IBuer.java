d416eda56c05a1727f4c9f35c925f5e9fd924c0e
obmetko <arimantik90@gmail.com>
2018-06-01 20:52:55 +0300

    jd02_03

---
+++ src/by/it/obmetko/jd02_03/IBuer.java
@@ -0,0 +1,10 @@
+package by.it.obmetko.jd01_01;
+
+public interface IBuer {
+    void chooseGoods();
+    void putGoodsToBacket();
+    void goQueue();
+    void goOut();
+    void takeBacket();
+    void enterToShop();
+}
