cc7b91bb2abac5eca11f7179671d80150e14d575
tarasiukvitali <tarasiukvitali@gmail.com>
2018-06-04 20:52:27 +0300

    jd02_05 var A

---
+++ src/by/it/tarasiuk/jd02_05/Message.java
@@ -0,0 +1,7 @@
+package by.it.tarasiuk.jd02_05;
+
+public interface Message {
+    String WELCOME="msg.welcome";
+    String QUESTION="msg.question";
+
+}
---
+++ src/by/it/tarasiuk/jd02_05/Runner.java
@@ -0,0 +1,16 @@
+package by.it.tarasiuk.jd02_05;
+
+import java.util.Locale;
+
+public class Runner {
+    public static void main(String[] args) {
+        ResMan rm = ResMan.getInstance();
+        if (args.length == 2) {
+            rm.setLocale(new Locale(args[0], args[1]));
+        }
+
+        System.out.println(rm.getString(Message.WELCOME));
+        System.out.println(rm.getString(Message.QUESTION));
+        System.out.printf("%s: %s", rm.getString(Res.AUTHOR), rm.getString(Res.NAME));
+    }
+}
