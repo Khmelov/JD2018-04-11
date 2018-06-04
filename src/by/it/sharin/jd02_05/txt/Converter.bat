e809d065991ccf10ca135aeb71ce58cc8660b053
sharinvo <sharinvo@gmail.com>
2018-06-04 21:02:24 +0300

    jd02_05 Classwork

---
+++ src/by/it/sharin/jd02_05/txt/Converter.bat
@@ -0,0 +1,3 @@
+native2ascii -encoding UTF-8 language.txt ..\lang\language.properties
+native2ascii -encoding UTF-8 language_ru_RU.txt ..\lang\language_ru_RU.properties
+native2ascii -encoding UTF-8 language_be_BY.txt ..\lang\language_be_BY.properties
