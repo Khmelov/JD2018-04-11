9753e3aca6d66a2dd612c3d488875e9c1fcf69d5
stshekh <stshekh@users.noreply.github.com>
2018-06-04 19:44:43 +0300

    jd02_04(Without tests for B,C)

--- src/by/it/shekh/calc/Parser.java
+++ src/by/it/shekh/calc/Parser.java
@@ -21,7 +21,7 @@ public class Parser {
 
     private int getNumberOp() {
         int position = -1;
-        int priorLevel = 1;
+        int priorLevel = -1;
 
         for (int i = 0; i < operations.size(); i++) {
             String currentOp = operations.get(i);
@@ -34,47 +34,25 @@ public class Parser {
     }
 
 
-//    private void debug(){
-//        System.out.println(operands.get(0));
-//        for (int i = 0; i <operations.size() ; i++) {
-//            System.out.println(operations.get(i)+operands.get(i+1));
-//        }
-//        System.out.println();
-//    }
-
-    Var calc(String expression) throws CalcException {
-        operands = new ArrayList<>(
-                Arrays.asList(expression.split(Patterns.OPERATION)));
-        Pattern patternOp = Pattern.compile(Patterns.OPERATION);
-        Matcher matcher = patternOp.matcher(expression);
-        operations = new ArrayList<>();
-        while (matcher.find()) {
-            operations.add(matcher.group());
+    private void debug() {
+        System.out.println(operands.get(0));
+        for (int i = 0; i < operations.size(); i++) {
+            System.out.println(operations.get(i) + operands.get(i + 1));
         }
-        Var res = null;
-        while (operations.size() > 0) {
-            int numOps = getNumberOp();
-            String left = operands.remove(numOps);
-            String operation = operations.remove(numOps);
-            String right = operands.get(numOps);
-            //debug();
-            res = oneOperation(left, operation, right);
-            operands.set(numOps, res.toString());
-        }
-        return res;
+        System.out.println();
     }
 
-
-    private Var oneOperation(String left, String operation, String right) throws CalcException {
-
+    private Var oneOperation(String left, String op, String right) throws CalcException {
         Var two = Var.createVar(right);
-        if (operation.equals("=")) {
+        if (op.equals("=")) {
             return Var.saveVar(left, two);
         }
         Var one = Var.createVar(left);
         if (one == null || two == null)
-            throw new CalcException(String.format("Невозможно обработать %s%s%s", left, operation, right)); //todo Create error
-        switch (operation) {
+            throw new CalcException(
+                    String.format("Невозможно обработать %s%s%s", left, op, right)
+            );
+        switch (op) {
             case "+":
                 return one.add(two);
             case "-":
@@ -85,8 +63,41 @@ public class Parser {
                 return one.div(two);
         }
 
-        throw new CalcException(String.format("Невозможно обработать %s%s%s", left, operation, right)); //todo Create error
+        throw new CalcException(
+                String.format("Неизвестная ошибка %s%s%s", left, op, right)
+        );
 
+    }
+
+    Var calc(String expression) throws CalcException {
+        Pattern brackets = Pattern.compile(Patterns.BRACKETS);
+        Matcher brMatch = brackets.matcher(expression);
+
+        while (brMatch.find()) {
+            expression = expression.replaceFirst(Patterns.BRACKETS, calc(brMatch.group(2)).toString());
+            brMatch.reset(expression);
+        }
+
+        operands = new ArrayList<>(
+                Arrays.asList(expression.split(Patterns.OPERATION))
+        );
+        Pattern patternOp = Pattern.compile(Patterns.OPERATION);
+        Matcher matcher = patternOp.matcher(expression);
+        operations = new ArrayList<>();
+        while (matcher.find()) {
+            operations.add(matcher.group());
+        }
+        Var res = null;
+        while (operations.size() > 0) {
+            int numOps = getNumberOp();
+            String operation = operations.remove(numOps);
+            String left = operands.get(numOps);
+            String right = operands.remove(numOps + 1);
+            //debug();
+            res = oneOperation(left, operation, right);
+            operands.set(numOps, res.toString());
+        }
+        return res;
     }
 
 }
--- src/by/it/shekh/calc/Test02_04.java
+++ src/by/it/shekh/calc/Test02_04.java
@@ -9,8 +9,17 @@ public class Test02_04 {
     @Test
     public void calc() throws Exception{
         Parser parser = new Parser();
-        Var var = parser.calc("A=2+5.3");
-        double res=Double.parseDouble(var.toString());
-        assertEquals("Ошибка для выражения",7.3,res,1e-20);
+        Var var1 = parser.calc("A=2+5.3");
+        double res1=Double.parseDouble(var1.toString());
+        assertEquals("Ошибка для выражения",7.3,res1,1e-20);
+        Var var2=parser.calc("B=3*-2.3");
+        double res2=Double.parseDouble(var2.toString());
+        assertEquals("Ошибка для выражения",-6.9,res2,1e-5);
+        Var var3=parser.calc("C=1213/3.1");
+        double res3=Double.parseDouble(var3.toString());
+        assertEquals("Ошибка для выражения",391.29,res3,1e-3);
+        Var var4=parser.calc("B=3--2.3");
+        double res4=Double.parseDouble(var4.toString());
+        assertEquals("Ошибка для выражения",5.3,res4,1e-20);
     }
 }
