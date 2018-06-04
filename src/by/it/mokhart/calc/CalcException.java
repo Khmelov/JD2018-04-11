99a345d78f0b89a7ef1dc0a14afd0f9fc0fdf80c
yurymokhart <mokhart.yury@gmail.com>
2018-06-04 17:03:22 +0300

    jd02_04

---
+++ src/by/it/mokhart/calc/CalcException.java
@@ -0,0 +1,24 @@
+package by.it.mokhart.calc;
+
+public class CalcException extends Exception{
+
+    public CalcException() {
+    }
+
+    public CalcException(String message) {
+        super("ERROR: "+message);
+    }
+
+    public CalcException(String message, Throwable cause) {
+        super("ERROR: "+message, cause);
+    }
+
+    public CalcException(Throwable cause) {
+        super(cause);
+    }
+
+//    @Override
+//    public String toString() {
+//        return this.getMessage();
+//    }
+}
