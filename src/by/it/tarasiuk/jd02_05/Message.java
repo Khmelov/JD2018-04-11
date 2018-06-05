e7eb9cd43d1a161716da8c25f704acf4e2781cdc
tarasiukvitali <tarasiukvitali@gmail.com>
2018-06-06 00:03:48 +0300

    jd02_05 var A, B

--- src/by/it/tarasiuk/jd02_05/Message.java
+++ src/by/it/tarasiuk/jd02_05/Message.java
@@ -3,5 +3,6 @@ package by.it.tarasiuk.jd02_05;
 public interface Message {
     String WELCOME="msg.welcome";
     String QUESTION="msg.question";
+    String REQUEST="msg.request";
 
 }
--- src/by/it/tarasiuk/jd02_05/Runner.java
+++ src/by/it/tarasiuk/jd02_05/Runner.java
@@ -1,10 +1,30 @@
 package by.it.tarasiuk.jd02_05;
 
+import java.text.DateFormat;
+import java.util.Date;
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Runner {
     public static void main(String[] args) {
         ResMan rm = ResMan.getInstance();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(rm.getString(Message.REQUEST));
+
+        if (scanner.next().equals("en")){
+            args[0]="en";
+            args[1]="US";
+        }
+        else if (scanner.next().equals("ru")) {
+            args[0]="ru";
+            args[1]="RU";
+        }
+        else if (scanner.next().equals("be")) {
+            args[0]="be";
+            args[1]="BY";
+        }
+
         if (args.length == 2) {
             rm.setLocale(new Locale(args[0], args[1]));
         }
@@ -12,5 +32,6 @@ public class Runner {
         System.out.println(rm.getString(Message.WELCOME));
         System.out.println(rm.getString(Message.QUESTION));
         System.out.printf("%s: %s", rm.getString(Res.AUTHOR), rm.getString(Res.NAME));
+        System.out.println(DateFormat.getDateInstance(DateFormat.FULL, Locale.forLanguageTag(args[1])).format(new Date()));
     }
 }
