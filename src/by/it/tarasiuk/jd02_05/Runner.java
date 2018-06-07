0e376315e91e9d1c9a202aab195db746c4e650dc
tarasiukvitali <tarasiukvitali@gmail.com>
2018-06-07 21:55:12 +0300

    jd02_05 var A, B

--- src/by/it/tarasiuk/jd02_05/Runner.java
+++ src/by/it/tarasiuk/jd02_05/Runner.java
@@ -8,30 +8,29 @@ import java.util.Scanner;
 public class Runner {
     public static void main(String[] args) {
         ResMan rm = ResMan.getInstance();
+        Locale locale = Locale.getDefault();
         Scanner scanner = new Scanner(System.in);
-
         System.out.println(rm.getString(Message.REQUEST));
+        String input;
 
-        if (scanner.next().equals("en")){
-            args[0]="en";
-            args[1]="US";
+        while (!(input = scanner.next()).equals("end")) {
+            switch (input) {
+                case "en":
+                    locale = new Locale("en", "US");
+                    break;
+                case "ru":
+                    locale = new Locale("ru", "RU");
+                    break;
+                case "be":
+                    locale = new Locale("be", "BY");
+                    break;
+            }
+            rm.setLocale(locale);
+            System.out.println(rm.getString(Message.WELCOME));
+            System.out.println(rm.getString(Message.QUESTION));
+            System.out.printf("%s: %s", rm.getString(Res.AUTHOR), rm.getString(Res.NAME));
+            System.out.println("\n" + DateFormat.getDateInstance(DateFormat.FULL, locale).format(new Date()));
+            System.out.println(rm.getString(Message.REQUEST));
         }
-        else if (scanner.next().equals("ru")) {
-            args[0]="ru";
-            args[1]="RU";
-        }
-        else if (scanner.next().equals("be")) {
-            args[0]="be";
-            args[1]="BY";
-        }
-
-        if (args.length == 2) {
-            rm.setLocale(new Locale(args[0], args[1]));
-        }
-
-        System.out.println(rm.getString(Message.WELCOME));
-        System.out.println(rm.getString(Message.QUESTION));
-        System.out.printf("%s: %s", rm.getString(Res.AUTHOR), rm.getString(Res.NAME));
-        System.out.println("\n"+DateFormat.getDateInstance(DateFormat.FULL, Locale.forLanguageTag(args[1])).format(new Date()));
     }
 }
