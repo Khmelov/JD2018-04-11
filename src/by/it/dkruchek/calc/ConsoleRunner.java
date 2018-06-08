0722a062ec9aa4696b4f35b6c08a15fabb460f65
Dmitriy.Kruchek <Dmitriy.Kruchek@life.com.by>
2018-06-08 11:42:15 +0300

    jd02_06

--- src/by/it/dkruchek/calc/ConsoleRunner.java
+++ src/by/it/dkruchek/calc/ConsoleRunner.java
@@ -9,36 +9,44 @@ import java.util.Scanner;
 public class ConsoleRunner {
 
     public static void main(String[] args) {
+        Logger logger = Logger.getLogger();
         ResMan rm = ResMan.getInstance();
         rm.setLocale(new Locale("en", "US"));
         Scanner sc = new Scanner(System.in);
         Parser parser = new Parser();
         Printer printer = new Printer();
+        ReportBuilder reporter = new ReportBuilder(true);
+        reporter.buildStartDate();
         System.out.println(rm.getString("msg.change") + " " + rm.getLocale());
         String line;
         while (!(line = sc.nextLine()).equals("end")){
+            reporter.buildOperation(line);
             switch (line){
                 case "be":
                     rm.setLocale(new Locale("be", "BY"));
-                    System.out.println(rm.getString("msg.change") + " " + rm.getLocale());
+                    //logger.log(rm.getString("msg.change") + " " + rm.getLocale(), LogLevel.INFO);
                     break;
                 case "ru":
                     rm.setLocale(new Locale("ru", "RU"));
-                    System.out.println(rm.getString("msg.change") + " " + rm.getLocale());
+                    //logger.log(rm.getString("msg.change") + " " + rm.getLocale(), LogLevel.INFO);
                     break;
                 case "en":
                     rm.setLocale(new Locale("en", "US"));
-                    System.out.println(rm.getString("msg.change") + " " + rm.getLocale());
+                    //logger.log(rm.getString("msg.change") + " " + rm.getLocale(), LogLevel.INFO);
                     break;
                 default:
                     try {
                         Var result = parser.calc(line);
                         printer.print(result);
+                        reporter.buildOperation(result.toString());
                     }
                     catch (CalcException e){
+                        reporter.buildException(e);
                         System.out.println(e.getMessage());
                     }
             }
         }
+        reporter.buildEndDate();
+        reporter.printReport();
     }
 }
