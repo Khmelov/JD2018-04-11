1085c99e46b54d5c5857b90a08fd1142599d3769
vnlanevich <vnlanevich@gmail.com>
2018-06-08 11:21:23 +0300

    jd02_05C (calc) на проверку

---
+++ src/by/it/lanevich/calc3/Var.java
@@ -0,0 +1,80 @@
+package by.it.lanevich.calc3;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeSet;
+import static by.it.lanevich.calc3.ConsoleRunner.rm;
+
+abstract class Var implements Operation {
+
+private static Map<String, Var> hashVar = new HashMap<>();
+
+    static void printVar(){
+
+        for(Map.Entry<String, Var> item :hashVar.entrySet()){
+            System.out.printf("%s=%s \n", item.getKey(), item.getValue());}
+    }
+
+    static void sortVar(){
+
+        Comparator<Map.Entry<String, Var>> comp = (Map.Entry<String, Var> o1, Map.Entry<String, Var> o2) -> (o1.getKey().compareTo(o2.getKey()));
+        TreeSet<Map.Entry<String, Var>> sorted = new TreeSet<>(comp);
+        sorted.addAll(hashVar.entrySet());
+        for (Map.Entry<String, Var> entry : sorted) {
+            System.out.println(entry.toString());
+        }
+    }
+
+    static Var saveVar (String key, Var var){
+    hashVar.put(key,var);
+    return var;
+
+}
+
+    @Override
+    public String toString() {
+        return rm.getString(Messages.ABSTRACTVAR);
+    }
+
+
+    static Var createVar(String operand) throws CalcException{
+       // operand=operand.replace("\\s+","").trim();
+        operand=operand.replaceAll("\\s+","").trim();
+        if(operand.matches(Patterns.SCALAR))
+            return new Scalar(operand);
+        if(operand.matches(Patterns.VECTOR))
+            return new Vector(operand);
+        if(operand.matches(Patterns.MATRIX))
+            return new Matrix(operand);
+        if (operand.matches(Patterns.VARNAME))
+            return hashVar.get(operand);
+        throw new CalcException(rm.getString(Messages.CREATEIMPOSSIBLE)+operand);
+
+    }
+
+
+    @Override
+    public Var add(Var other) throws CalcException{
+        throw new CalcException(rm.getString(Messages.ADD) + this + "+" + other + rm.getString(Messages.IMPOSSIBLE));
+         }
+
+    @Override
+    public Var sub(Var other) throws CalcException{
+        throw new CalcException(rm.getString(Messages.SUB) + this + "-" + other + rm.getString(Messages.IMPOSSIBLE));
+
+    }
+
+    @Override
+    public Var mul(Var other) throws CalcException{
+        throw new CalcException(rm.getString(Messages.MUL) + this + "*" + other + rm.getString(Messages.IMPOSSIBLE));
+
+    }
+
+    @Override
+    public Var div(Var other) throws CalcException{
+        throw new CalcException(rm.getString(Messages.DIV) + this + "/" + other + rm.getString(Messages.IMPOSSIBLE));
+
+    }
+}
+
