1b26d31d046bd6ecc51fc7b717c2e2b8c2df53f8
KrivenkovaNatasha <werden12>
2018-06-09 16:42:15 +0300

    file

--- src/by/it/krivenkova/jd02_05/txt/converter.bat
+++ src/by/it/krivenkova/jd02_05/txt/converter.bat
@@ -1,4 +1,3 @@
-native2ascii -encoding UTF-8 language.txt ..\lang\language.property
-native2ascii -encoding UTF-8 language_ru_RU.txt ..\lang\language_ru_RU.property
-native2ascii -encoding UTF-8 language_be_BY.txt ..\lang\language_be_BY.property
-native2ascii -encoding UTF-8 language_en_US.txt ..\lang\language_en_US.property
+FOR %%I In (*.txt) DO (
+native2ascii -encoding UTF-8 %%I ..\lang\%%~nI.properties
+)
