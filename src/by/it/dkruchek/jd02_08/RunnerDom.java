4986b839d5e15a9a382c6ff6b453765603979ce4
caveywoolpert <dmitri.kruchek@gmail.com>
2018-06-13 14:02:00 +0300

    jd02_08

--- src/by/it/dkruchek/jd02_08/RunnerDom.java
+++ src/by/it/dkruchek/jd02_08/RunnerDom.java
@@ -1,9 +1,6 @@
 package by.it.dkruchek.jd02_08;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -21,21 +18,38 @@ public class RunnerDom {
             builder = factory.newDocumentBuilder();
             Document document = builder.parse(fileName);
             Element root = document.getDocumentElement();
-            printDom(root);
+            printDom("", root);
         } catch (ParserConfigurationException | SAXException | IOException e) {
             e.printStackTrace();
         }
     }
 
-
-    static void printDom(Node root){
-        String value = root.getNodeValue();
-        if (value != null){
-            System.out.println(root.getNodeName());
-        }
-        NodeList childNodes = root.getChildNodes();
-        for (int i = 0; i < childNodes.getLength(); i++) {
-            printDom(childNodes.item(i));
+    static void printDom(String prefix, Node root){
+        if (root.getNodeType() == Node.ELEMENT_NODE) {
+            System.out.print(prefix + "<" + root.getNodeName());
+            //print node content if any
+              //print attributes if any
+            if (root.hasAttributes()) {
+                NamedNodeMap attr = root.getAttributes();
+                for (int i = 0; i < attr.getLength(); i++) {
+                    Node attribute = attr.item(i);
+                    System.out.print(" " + attribute.getNodeName() + "=\"" + attribute.getNodeValue() + "\"");
+                }
+            }
+            System.out.println(">");
+            if (root.hasChildNodes()) {
+                String content = root.getFirstChild().getTextContent().trim();
+                if (!content.isEmpty())
+                    System.out.println(prefix + "\t" + content);
+            }
+            // print all child nodes recursively
+            prefix = prefix.concat("\t");
+            NodeList childNodes = root.getChildNodes();
+            for (int i = 0; i < childNodes.getLength(); i++) {
+                printDom(prefix, childNodes.item(i));
+            }
+            prefix = prefix.substring(1);
+            System.out.println(prefix + "</" + root.getNodeName() + ">");
         }
     }
 }
--- src/by/it/dkruchek/jd02_08/TaskA.java
+++
@@ -1,6 +0,0 @@
-package by.it.dkruchek.jd02_08;
-
-public class TaskA {
-
-
-}
