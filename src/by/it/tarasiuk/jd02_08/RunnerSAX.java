b28d1666aed93005944974ed3197d07297a9ef40
tarasiukvitali <tarasiukvitali@gmail.com>
2018-06-16 17:01:38 +0300

    jd02_08 var A

--- src/by/it/tarasiuk/jd02_08/RunnerSAX.java
+++ src/by/it/tarasiuk/jd02_08/RunnerSAX.java
@@ -1,23 +1,24 @@
 package by.it.tarasiuk.jd02_08;
 
-import jdk.internal.org.xml.sax.helpers.DefaultHandler;
 import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 import java.io.File;
+import java.io.IOException;
 
 public class RunnerSAX {
-    private static String xmlFile = "src/by/it/tarasiuk/jd02_07/Persons+xsd.xml";
+    private static String xmlFile = "src/by/it/tarasiuk/jd02_07/Sellers.xml";
 
     public static void main(String[] args) {
         try {
+            DefaultHandler handler = new MyHandler();
             SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
             SAXParser saxParser = saxParserFactory.newSAXParser();
-            DefaultHandler handler = new DefaultHandler();
             saxParser.parse(new File(xmlFile), handler);
-        } catch (ParserConfigurationException | SAXException e) {
+        } catch (ParserConfigurationException | SAXException | IOException e) {
             e.printStackTrace();
         }
     }
