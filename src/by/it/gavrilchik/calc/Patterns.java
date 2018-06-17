1586cd151a5b494aba9456ca31c0534263e7e8d3
Gavrilchik <ivan.gavrilchik@gmail.com>
2018-06-17 16:17:57 +0300

    tasks 02-09,02-08,02-07,

---
+++ src/by/it/gavrilchik/calc/Patterns.java
@@ -0,0 +1,8 @@
+package by.it.gavrilchik.calc;
+
+abstract class Patterns {
+    final static String OPERATION = "(?<=[^+/*={,-])[-=+*/]";
+    final static String SCALAR = "-?[0-9]+\\.?[0-9]*";
+    final static String VECTOR = "\\{(-?[0-9]+\\.?[0-9]*,?)+}";
+    final static String MATRIX = "\\{(\\{(-?[0-9]+\\.?[0-9]*,?)+},?)+}";
+}
