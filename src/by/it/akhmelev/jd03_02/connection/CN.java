cc4fa3a9d4bcae9d16ffc5255f3468f33c39f10e
demologin <demo@it-academy.by>
2018-06-18 21:19:18 +0300

    jd03_02 classwork

--- src/by/it/akhmelev/jd03_02/connection/CN.java
+++ src/by/it/akhmelev/jd03_02/connection/CN.java
@@ -1,7 +1,8 @@
 package by.it.akhmelev.jd03_02.connection;
 
 public interface CN {
-    String DB_URL = "jdbc:mysql://127.0.0.1:2016/";
+    String DB_URL = "jdbc:mysql://127.0.0.1:2016/akhmelev"
+            + "?useUnicode=true&characterEncoding=UTF-8";
     String DB_USER = "root";
     String DB_PASSWORD = "";
 
