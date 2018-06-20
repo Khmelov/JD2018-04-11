bec38afc8963edcf776e193678a26dae60a9589f
a040985 <a040985@gmail.com>
2018-06-20 15:18:12 +0300

    jd03_01 TaskC

--- src/by/it/dkruchek/jd02_09/TaskB.java
+++ src/by/it/dkruchek/jd02_09/TaskB.java
@@ -10,58 +10,58 @@ import javax.xml.bind.Unmarshaller;
 import java.io.File;
 
 public class TaskB {
-
-    public static Persons persons;
-    private static final String XML = "src/by/it/dkruchek/jd02_09/Persons_xsd.xml";
-    private static final String XML_OUT = "src/by/it/dkruchek/jd02_09/Persons_out.xml";
-
-
-    static void readXml() {
-        try {
-            JAXBContext context = JAXBContext.newInstance(Persons.class);
-            Unmarshaller unmarshaller = context.createUnmarshaller();
-            persons = (Persons) unmarshaller.unmarshal(new File(XML));
-        } catch (JAXBException e) {
-            e.printStackTrace();
-        }
-    }
-    static String getJson() {
-        Gson gson = new GsonBuilder()
-                .serializeNulls()
-                .setPrettyPrinting()
-                .create();
-        String json = gson.toJson(persons);
-
-        return json;
-    }
-
-    static void readJson(String json) {
-
-        Gson gson = new GsonBuilder()
-            .serializeNulls()
-            .setPrettyPrinting()
-            .create();
-        persons = gson.fromJson(json, Persons.class);
-    }
-
-    static void saveXml(){
-        try {
-            JAXBContext context = JAXBContext.newInstance(Persons.class);
-            Marshaller marshaller = context.createMarshaller();
-            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-            marshaller.marshal(persons, new File("src/by/it/dkruchek/jd02_09/Persons_out.xml"));
-        } catch (JAXBException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void main(String[] args) {
-        readXml();
-        String json = getJson();
-        System.out.println(json);
-        readJson(json);
-        saveXml();
-
-
-    }
+//
+//    public static Persons persons;
+//    private static final String XML = "src/by/it/dkruchek/jd02_09/Persons_xsd.xml";
+//    private static final String XML_OUT = "src/by/it/dkruchek/jd02_09/Persons_out.xml";
+//
+//
+//    static void readXml() {
+//        try {
+//            JAXBContext context = JAXBContext.newInstance(Persons.class);
+//            Unmarshaller unmarshaller = context.createUnmarshaller();
+//            persons = (Persons) unmarshaller.unmarshal(new File(XML));
+//        } catch (JAXBException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//    static String getJson() {
+//        Gson gson = new GsonBuilder()
+//                .serializeNulls()
+//                .setPrettyPrinting()
+//                .create();
+//        String json = gson.toJson(persons);
+//
+//        return json;
+//    }
+//
+//    static void readJson(String json) {
+//
+//        Gson gson = new GsonBuilder()
+//            .serializeNulls()
+//            .setPrettyPrinting()
+//            .create();
+//        persons = gson.fromJson(json, Persons.class);
+//    }
+//
+//    static void saveXml(){
+//        try {
+//            JAXBContext context = JAXBContext.newInstance(Persons.class);
+//            Marshaller marshaller = context.createMarshaller();
+//            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+//            marshaller.marshal(persons, new File("src/by/it/dkruchek/jd02_09/Persons_out.xml"));
+//        } catch (JAXBException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//        readXml();
+//        String json = getJson();
+//        System.out.println(json);
+//        readJson(json);
+//        saveXml();
+//
+//
+//    }
 }
---
+++ src/by/it/zaleschonok/jd02_01/IUseBacket.java
@@ -0,0 +1,8 @@
+package by.it.zaleschonok.jd02_01;
+
+
+
+public interface IUseBacket {
+    void takeBacket();
+    void putGoodsToBacket();
+}
