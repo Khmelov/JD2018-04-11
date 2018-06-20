b46031b1f998cbcc052b91edca5d634b22fae7d8
Shumik <shumik@it-academy.by>
2018-06-20 08:08:44 +0300

    0302_TaskABC

--- src/by/it/shumilov/jd03_02/beans/User.java
+++ src/by/it/shumilov/jd03_02/beans/User.java
@@ -38,11 +38,40 @@ public class User {
         return password;
     }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setRoles_id(long roles_id) {
+        this.roles_id = roles_id;
+    }
+
     public String getEmail() {
         return email;
     }
 
     public long getRoles_id() {
         return roles_id;
+    }
+
+    @Override
+    public String toString() {
+        return "User{id=" + id
+                + ", login="+login
+                +", password=" + password
+                +", email="+email
+                +", roles_id="+ roles_id+"}";
     }
 }
--- src/by/it/shumilov/jd03_02/crud/CrudUser.java
+++ src/by/it/shumilov/jd03_02/crud/CrudUser.java
@@ -4,23 +4,79 @@ import by.it.shumilov.jd03_02.beans.User;
 import by.it.shumilov.jd03_02.connection.DbConnection;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Locale;
 
 public class CrudUser {
 
-    User read(long id) throws SQLException {
+
+    public boolean create(User user) throws SQLException {
+
+        try(
+                Connection connection = DbConnection.getConnection();
+                Statement statement = connection.createStatement()){
+
+
+            String sql = String.format(Locale.US, "INSERT INTO `dbusers` (`login`, `password`, `email`, `dbRoles_id`) VALUES ('%s','%s','%s','%d')",
+                    user.getLogin(), user.getPassword(), user.getEmail(), user.getRoles_id());
+
+            return 1==statement.executeUpdate(sql);
+        }
+
+
+    }
+
+
+    public User read(long id) throws SQLException {
         User user = null;
 
         try(
         Connection connection = DbConnection.getConnection();
-        Statement statement = connection.createStatement();){
-            String sql = String.format(Locale.US, " SELECT * FROM `dbusers` WHERE ID=%d", id);
+        Statement statement = connection.createStatement()){
+            String sql = String.format(Locale.US, " SELECT  `id`, `login`, `password`, `email`, `dbRoles_id`  FROM `dbusers` WHERE id=%d", id);
+            ResultSet resultSet = statement.executeQuery(sql);
+            if(resultSet.next()){
+                user = new User(
+                        resultSet.getLong("id"),
+                        resultSet.getString("login"),
+                        resultSet.getString("password"),
+                        resultSet.getString("email"),
+                        resultSet.getLong("dbRoles_id")  );
 
+            }
         }
+
         return  user;
     }
 
+    public boolean update(User user) throws SQLException {
 
+        try(
+                Connection connection = DbConnection.getConnection();
+                Statement statement = connection.createStatement()){
+
+            String sql = String.format(Locale.US, " UPDATE  `dbusers` SET  `login`='%s', `password`='%s', `email`='%s', `dbRoles_id`=%d  WHERE id=%d",
+                    user.getLogin(), user.getPassword(), user.getEmail(), user.getRoles_id(), user.getId());
+
+            return 1==statement.executeUpdate(sql);
+        }
+
+
+    }
+
+    public boolean delete(User user) throws SQLException {
+
+        try(
+                Connection connection = DbConnection.getConnection();
+                Statement statement = connection.createStatement()){
+            String sql = String.format(Locale.US, " DELETE FROM `dbusers`  WHERE id=%d", user.getId());
+
+
+            return 1==statement.executeUpdate(sql);
+        }
+
+
+    }
 }
