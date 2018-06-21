6bab4083eae18205e8c7dd2e9d21465ae53b9d5d
sharinvo <sharinvo@gmail.com>
2018-06-21 21:54:57 +0300

    jd03_01 Homework

--- src/by/it/sharin/jd03_01/C_Init.java
+++ src/by/it/sharin/jd03_01/C_Init.java
@@ -25,14 +25,14 @@ public class C_Init{
             statement.executeUpdate("CREATE SCHEMA IF NOT EXISTS `sharin` DEFAULT CHARACTER SET utf8 ;");
             statement.executeUpdate("CREATE TABLE IF NOT EXISTS `sharin`.`roles` (\n" +
                     "  `id` INT NOT NULL AUTO_INCREMENT,\n" +
-                    "  `role` VARCHAR(100) NULL,\n" +
+                    "  `role` VARCHAR(45) NULL,\n" +
                     "  PRIMARY KEY (`id`))\n" +
                     "ENGINE = InnoDB;");
             statement.executeUpdate("CREATE TABLE IF NOT EXISTS `sharin`.`users` (\n" +
                     "  `id` INT NOT NULL AUTO_INCREMENT,\n" +
                     "  `login` VARCHAR(45) NULL,\n" +
                     "  `password` VARCHAR(45) NULL,\n" +
-                    "  `email` VARCHAR(100) NULL,\n" +
+                    "  `email` VARCHAR(45) NULL,\n" +
                     "  `roles_id` INT NOT NULL,\n" +
                     "  PRIMARY KEY (`id`),\n" +
                     "  INDEX `fk_users_roles_idx` (`roles_id` ASC),\n" +
@@ -44,13 +44,9 @@ public class C_Init{
                     "ENGINE = InnoDB;");
             statement.executeUpdate("CREATE TABLE IF NOT EXISTS `sharin`.`ads` (\n" +
                     "  `id` INT NOT NULL AUTO_INCREMENT,\n" +
-                    "  `description` VARCHAR(5000) NULL,\n" +
-                    "  `flat` INT NULL,\n" +
-                    "  `flats` INT NULL,\n" +
-                    "  `rooms` INT NULL,\n" +
-                    "  `area` DOUBLE NULL,\n" +
+                    "  `marka` VARCHAR(45) NULL,\n" +
+                    "  `model` VARCHAR(45) NULL,\n" +
                     "  `price` DOUBLE NULL,\n" +
-                    "  `address` VARCHAR(500) NULL,\n" +
                     "  `users_id` INT NOT NULL,\n" +
                     "  PRIMARY KEY (`id`),\n" +
                     "  INDEX `fk_ads_users1_idx` (`users_id` ASC),\n" +
@@ -65,7 +61,7 @@ public class C_Init{
             statement.executeUpdate("INSERT INTO `sharin`.`roles` (`id`, `role`) VALUES (DEFAULT, 'guest');");
             statement.executeUpdate("INSERT INTO `sharin`.`users` (`id`, `login`, `password`, `email`, `roles_id`) VALUES (DEFAULT, 'admin', 'padmin', 'admin@it.by', 1);");
             statement.executeUpdate("INSERT INTO `sharin`.`users` (`id`, `login`, `password`, `email`, `roles_id`) VALUES (DEFAULT, 'users', 'puser', 'user@it.by', 2);");
-            statement.executeUpdate("INSERT INTO `sharin`.`ads` (`id`, `description`, `flat`, `flats`, `rooms`, `area`, `price`, `address`, `users_id`) VALUES (DEFAULT, 'Test appartment', 1, 5, 2, 44, 66666, 'Minsk Lenina 1, app 1', 2);");
+            statement.executeUpdate("INSERT INTO `sharin`.`ads` (`id`, `marka`, `model`, `price`, `users_id`) VALUES (DEFAULT, 'Skoda', 'Rapid', 18000, 2);");
         } catch (SQLException e) {
             e.printStackTrace();
         }
