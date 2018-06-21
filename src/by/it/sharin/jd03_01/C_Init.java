d7774aa937e70f8a49f0c1aa71d8cb3b8c295fd6
sharinvo <sharinvo@gmail.com>
2018-06-21 20:33:34 +0300

    jd03_01 Homework

--- src/by/it/sharin/jd03_01/C_Init.java
+++ src/by/it/sharin/jd03_01/C_Init.java
@@ -1,14 +1,13 @@
 package by.it.sharin.jd03_01;
 
-public class C_Init {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-    static final String dbUrl = "jdbc:mysql://127.0.0.1:2016/";
+public class C_Init{
 
-    static final String dbUser = "root";
-
-    static final String dbPassword = "";
-
-    static  {
+    static {
         try {
             Class.forName("com.mysql.jdbc.Driver");
         } catch (ClassNotFoundException e) {
@@ -17,6 +16,59 @@ public class C_Init {
     }
 
     public static void main(String[] args) {
+        try (
+                Connection connection =
+                        DriverManager.getConnection(CN.DB_URL, CN.DB_USER, CN.DB_PASSWORD);
+                Statement statement = connection.createStatement()
+        ) {
+            statement.executeUpdate("DROP SCHEMA IF EXISTS `sharin` ;");
+            statement.executeUpdate("CREATE SCHEMA IF NOT EXISTS `sharin` DEFAULT CHARACTER SET utf8 ;");
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS `sharin`.`roles` (\n" +
+                    "  `id` INT NOT NULL AUTO_INCREMENT,\n" +
+                    "  `role` VARCHAR(100) NULL,\n" +
+                    "  PRIMARY KEY (`id`))\n" +
+                    "ENGINE = InnoDB;");
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS `sharin`.`users` (\n" +
+                    "  `id` INT NOT NULL AUTO_INCREMENT,\n" +
+                    "  `login` VARCHAR(45) NULL,\n" +
+                    "  `password` VARCHAR(45) NULL,\n" +
+                    "  `email` VARCHAR(100) NULL,\n" +
+                    "  `roles_id` INT NOT NULL,\n" +
+                    "  PRIMARY KEY (`id`),\n" +
+                    "  INDEX `fk_users_roles_idx` (`roles_id` ASC),\n" +
+                    "  CONSTRAINT `fk_users_roles`\n" +
+                    "    FOREIGN KEY (`roles_id`)\n" +
+                    "    REFERENCES `sharin`.`roles` (`id`)\n" +
+                    "    ON DELETE RESTRICT\n" +
+                    "    ON UPDATE RESTRICT)\n" +
+                    "ENGINE = InnoDB;");
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS `sharin`.`ads` (\n" +
+                    "  `id` INT NOT NULL AUTO_INCREMENT,\n" +
+                    "  `description` VARCHAR(5000) NULL,\n" +
+                    "  `flat` INT NULL,\n" +
+                    "  `flats` INT NULL,\n" +
+                    "  `rooms` INT NULL,\n" +
+                    "  `area` DOUBLE NULL,\n" +
+                    "  `price` DOUBLE NULL,\n" +
+                    "  `address` VARCHAR(500) NULL,\n" +
+                    "  `users_id` INT NOT NULL,\n" +
+                    "  PRIMARY KEY (`id`),\n" +
+                    "  INDEX `fk_ads_users1_idx` (`users_id` ASC),\n" +
+                    "  CONSTRAINT `fk_ads_users1`\n" +
+                    "    FOREIGN KEY (`users_id`)\n" +
+                    "    REFERENCES `sharin`.`users` (`id`)\n" +
+                    "    ON DELETE CASCADE\n" +
+                    "    ON UPDATE CASCADE)\n" +
+                    "ENGINE = InnoDB;");
+            statement.executeUpdate("INSERT INTO `sharin`.`roles` (`id`, `role`) VALUES (DEFAULT, 'admin');\n");
+            statement.executeUpdate("INSERT INTO `sharin`.`roles` (`id`, `role`) VALUES (DEFAULT, 'user');");
+            statement.executeUpdate("INSERT INTO `sharin`.`roles` (`id`, `role`) VALUES (DEFAULT, 'guest');");
+            statement.executeUpdate("INSERT INTO `sharin`.`users` (`id`, `login`, `password`, `email`, `roles_id`) VALUES (DEFAULT, 'admin', 'padmin', 'admin@it.by', 1);");
+            statement.executeUpdate("INSERT INTO `sharin`.`users` (`id`, `login`, `password`, `email`, `roles_id`) VALUES (DEFAULT, 'users', 'puser', 'user@it.by', 2);");
+            statement.executeUpdate("INSERT INTO `sharin`.`ads` (`id`, `description`, `flat`, `flats`, `rooms`, `area`, `price`, `address`, `users_id`) VALUES (DEFAULT, 'Test appartment', 1, 5, 2, 44, 66666, 'Minsk Lenina 1, app 1', 2);");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 }
