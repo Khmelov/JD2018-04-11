3a1e48b9b99ae45076224d4eae983d60c9d16edd
AlexandrRogov <rohaualexandr@gmail.com>
2018-06-21 14:15:32 +0300

    Task 03_02

---
+++ src/by/it/rogov/jd03_02/beans/Role.java
@@ -0,0 +1,16 @@
+package by.it.rogov.jd03_02.beans;
+
+public class Role {
+    long id;
+    String role;
+
+    public Role() {
+    }
+
+    public Role(long id, String role) {
+        this.id = id;
+        this.role = role;
+    }
+
+
+}
---
+++ src/by/it/rogov/jd03_02/beans/User.java
@@ -0,0 +1,79 @@
+package by.it.rogov.jd03_02.beans;
+
+public class User {
+
+    long id;
+    String login;
+    String password;
+    String email;
+    int phone;
+    long roles_id;
+
+    public User() {
+    }
+
+    public User(long id, String login, String password, String email, int phone, long roles_id) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.email = email;
+        this.phone = phone;
+        this.roles_id = roles_id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public long getRoles_id() {
+        return roles_id;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", phone=" + phone +
+                ", roles_id=" + roles_id +
+                '}';
+    }
+}
