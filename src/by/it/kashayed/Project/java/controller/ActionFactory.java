57a1a6f6f27b8b9c0408828ffaf337351a1d2404
Yaroslav <rayrossed@gmail.com>
2018-06-22 21:22:54 +0300

    claSSWORK

--- src/by/it/kashayed/Project/java/controller/ActionFactory.java
+++ src/by/it/kashayed/Project/java/controller/ActionFactory.java
@@ -5,7 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 public class ActionFactory {
     Actions defiAction(HttpServletRequest request) {
         String command = request.getParameter("command");
-        if (command == null && !command.isEmpty()) {
+        if (command != null && !command.isEmpty()) {
             return Actions.valueOf(command.toUpperCase());
         } else
             return Actions.ERROR;
