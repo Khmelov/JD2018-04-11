73d9a9f28cbcfb67e7cb46facd4ef17706301a55
PavelKurmaz <kurmaz.pavel@gmail.com>
2018-06-22 21:21:11 +0300

    project start 01

---
+++ src/by/it/kurmaz/project/java/controller/CmdSignUp.java
@@ -0,0 +1,10 @@
+package by.it.kurmaz.project.java.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class CmdSignUp extends Cmd {
+    @Override
+    Cmd execute(HttpServletRequest req) {
+        return null;
+    }
+}
