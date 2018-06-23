e2461adedda765fff930f4182ee2636bc43db42f
AlexandrRogov <rohaualexandr@gmail.com>
2018-06-23 15:41:45 +0300

    Task 03_04

---
+++ src/by/it/rogov/jd03_04/java/controller/CmdLogin.java
@@ -0,0 +1,10 @@
+package by.it.rogov.jd03_04.java.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class CmdLogin extends Cmd {
+    @Override
+    Cmd execute(HttpServletRequest req) {
+        return null;
+    }
+}
