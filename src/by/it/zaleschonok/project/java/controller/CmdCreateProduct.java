e8e271b1c2b5f73b92d3757b912f194c6e1f660c
a040985 <a040985@gmail.com>
2018-06-25 12:31:32 +0300

    jd03_04 TaskAB

---
+++ src/by/it/zaleschonok/project/java/controller/CmdCreateProduct.java
@@ -0,0 +1,11 @@
+package by.it.zaleschonok.project.java.controller;
+
+
+import javax.servlet.http.HttpServletRequest;
+
+public class CmdCreateProduct extends Cmd {
+    @Override
+    Cmd execute(HttpServletRequest req) {
+        return null;
+    }
+}
