2c0542c7215df5caa80aace0cb8d8c945c3c67e1
tarasiukvitali <tarasiukvitali@gmail.com>
2018-06-26 21:58:20 +0300

    jd03_03

--- src/by/it/tarasiuk/jd03_03/dao/DaoRole.java
+++
@@ -1,9 +0,0 @@
-package by.it.tarasiuk.jd03_03.dao;
-
-import by.it.tarasiuk.jd03_03.beans.Role;
-
-public class DaoRole extends UniversalDAO<Role> {
-    public DaoRole(){
-        super(new Role(),"roles");
-    }
-}
--- src/by/it/tarasiuk/project/java/controller/Error.java
+++ src/by/it/tarasiuk/project/java/controller/Error.java
@@ -1,10 +1,10 @@
-package by.it.tarasiuk.project.java.controller;
-
-import javax.servlet.http.HttpServletRequest;
-
-public class Error extends Cmd {
-    @Override
-    Cmd execute(HttpServletRequest req) {
-        return null;
-    }
-}
+//package by.it.tarasiuk.project.java.controller;
+//
+//import javax.servlet.http.HttpServletRequest;
+//
+//public class Error extends Cmd {
+//    @Override
+//    Cmd execute(HttpServletRequest req) {
+//        return null;
+//    }
+//}
