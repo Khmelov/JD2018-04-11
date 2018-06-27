a2a64e073d8e429aaf219f2be4fdbf40189dda4a
alinazakharenko <alinazaharenko@gmail.com>
2018-06-27 16:41:37 +0300

    jd03_05

--- src/by/it/zakharenko/project/java/controller/CmdError.java
+++ src/by/it/zakharenko/project/java/controller/CmdError.java
@@ -2,9 +2,9 @@ package by.it.zakharenko.project.java.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
-public class CmdError extends Cmd {
+class CmdError extends Cmd {
     @Override
-    Cmd execute(HttpServletRequest req) {
+    Action execute(HttpServletRequest req) {
         return null;
     }
 }
