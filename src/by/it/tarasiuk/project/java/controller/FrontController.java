c90b74ea5af35ec1f7d31337cf86a784b8606ee1
tarasiukvitali <tarasiukvitali@gmail.com>
2018-06-29 04:22:57 +0300

    jd03_04

--- src/by/it/tarasiuk/project/java/controller/FrontController.java
+++ src/by/it/tarasiuk/project/java/controller/FrontController.java
@@ -1,5 +1,7 @@
 package by.it.tarasiuk.project.java.controller;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -7,6 +9,14 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class FrontController extends HttpServlet {
+
+    ActionFactory actionFactory;
+
+    @Override
+    public void init() throws ServletException {
+        actionFactory = new ActionFactory();
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         serv(req, resp);
@@ -18,6 +28,14 @@ public class FrontController extends HttpServlet {
     }
 
     private void serv(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-resp.getWriter().write("FrontController");
+        Action action = actionFactory.defineAction(req);
+        Cmd nextAction = action.cmd.execute(req);
+        if (nextAction == null) {
+            ServletContext servletContext = getServletContext();
+            RequestDispatcher requestDispatcher = servletContext.getRequestDispatcher(action.jsp);
+            requestDispatcher.forward(req, resp);
+        } else {
+            resp.sendRedirect("do?command=" + nextAction.toString().toLowerCase());
+        }
     }
 }
