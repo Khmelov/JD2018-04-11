8f6aac9ca9e13df503f6cde79b3650ab9f7f33f9
KrivenkovaNatasha <werden12>
2018-07-05 22:23:06 +0300

    project 07

--- src/by/it/krivenkova/project/java/controller/CmdCreateRoute.java
+++ src/by/it/krivenkova/project/java/controller/CmdCreateRoute.java
@@ -11,23 +11,30 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.sql.SQLException;
 
-public class CmdCreateRoute /*extends Cmd*/ {
-    /*@Override
-    public Cmd execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
-        User user = Util.getUserFromSession(req);
-        if (user != null) {
-            if (Util.isPost(req)) {
-                Route route = new Route();
-                route.setDepTown(req.getParameter("name"));
-                route.setDescription(req.getParameter("description"));
-                route.setPrice(Double.parseDouble(req.getParameter("price")));
-
-
-
-                route.setUsers_id((int) user.getId());
-                Dao.getInstance().route.create(route);
-            } else return null;
+public class CmdCreateRoute extends Cmd {
+    @Override
+    Actions execute(HttpServletRequest req) throws SQLException {
+        HttpSession session = req.getSession();
+        Object oUser = session.getAttribute("user");
+        if (oUser == null)
+            return Actions.LOGIN;
+        User user = (User) oUser;
+        if (Util.isPost(req)) {
+            String depTown = Util.getString(req, "depTown");
+            String depDate = Util.getString(req, "depDate");
+            String depTime = Util.getString(req, "depTime");
+            String arrTown = Util.getString(req, "arrTown");
+            double price = Util.getDouble(req, "price");
+            int numTickets = Util.getInteger(req, "numTickets");
+            Route route = new Route(0,
+                    depTown,
+                    depDate, depTime,
+                    arrTown,
+                    price, numTickets,
+                    user.getId());
+            Dao.getDao().route.create(route);
+            return Actions.PROFILE;
         }
-        return Actions.PROFILE.cmd;
-    }*/
+        return null;
+    }
 }
